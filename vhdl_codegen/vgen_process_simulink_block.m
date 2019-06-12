3d3dd3237d44a4f4b212fdf9952bdd40953f0e93
Trevor Vannoy <vannoy@flatearthinc.com>
2019-06-12 08:34:46 -0600

    clean up json structure and add a new example json file

--- vhdl_codegen/vgen_process_simulink_block.m
+++ vhdl_codegen/vgen_process_simulink_block.m
@@ -2,14 +2,11 @@ avalon = vgen_get_simulink_block_interfaces();
 % Note: The model simulation needs to be run first before this function
 % is called since there are workspace variables that need to be set
 % in the initiallization callback function
+
+%% Get system clock frequency
+% the init function has to be called for the Fs_system and Ts_system variables to show up
 DataPlane_callback_init
-vhdl = vgen_generate_VHDL_component(avalon);
-avalon.vhdl = vhdl;
-disp(avalon.vhdl.component_declaration)
-disp(avalon.vhdl.component_instantiation)
-disp(avalon.vhdl.register_defaults)
+avalon.clock = struct('frequency', Fs_system, 'period', Ts_system);
 
-vhdl
-
-
-
+%% Save the avalon structure to a json file
+writejson(avalon, [avalon.entity,'.json'])
