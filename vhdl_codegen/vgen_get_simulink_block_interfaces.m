6102e59ae7088e78a3b15c1d0aeef1f13f6182ff
Ross <Ross@RossPCatFE>
2019-06-17 17:32:15 -0600

    fixed the error where index was selecting an empty struct

--- vhdl_codegen/vgen_get_simulink_block_interfaces.m
+++ vhdl_codegen/vgen_get_simulink_block_interfaces.m
@@ -3,15 +3,17 @@ function avalon1 = vgen_get_simulink_block_interfaces()
 % is called since there are workspace variables that need to be set
 % in the initiallization callback function
 
-%------------------------------------------------
-% Put the model in compile mode 
-% needed to get the CompiledPortDataTypes
+%--------------------------------------------------------------------------
+% Put the model in compile mode  needed to get the CompiledPortDataTypes
 % https://www.mathworks.com/matlabcentral/answers/8679-how-to-get-the-port-types-and-dimensions-for-a-block
-%------------------------------------------------
+%--------------------------------------------------------------------------
 modelName = bdroot;  % get model name
+disp(['Placing the model in compile mode.'])
+disp(['     If for some reason an error occurs, you will need to manually terminate the compile mode'])
+disp(['     by executing the command: eval([<modelName>,''([],[],[],''''term'''');'']);  % terminate the compile mode'])
 eval([modelName,'([],[],[],''compile'');']);  % put in compile mode
 
-avalon1 = [];
+avalon1.model_name = modelName;
 %----------------------------------
 % Get Avalon Streaming Sink Info
 %----------------------------------
@@ -19,28 +21,28 @@ avalon_sink_signals = find_system('SearchDepth','2','regexp','on','BlockType','I
 if (isempty(avalon_sink_signals)==0) % The avalon streaming sink exists
     avalon1.avalon_sink_flag = 1;
     Na = length(avalon_sink_signals);
+    index = 1;
     for i=1:Na
-        h = getSimulinkBlockHandle(avalon_sink_signals{i});  % get block handle
-        if i==1 % get parent name, which will be the entity name
-            parent = get_param(h,'Parent');  % get parent name with path
-            split = strsplit(parent,'/');
-            avalon1.entity = split{end};
+        signal_name = avalon_sink_signals{i};
+        ind = strfind(signal_name,'gm_');  % if the signal name contains gm_ we will ignore it (generated model stuff)
+        if sum(size(ind)) == 0 % signal name doesn't contain 'gm_' so process it
+            h = getSimulinkBlockHandle(signal_name);  % get block handle
+            if index==1 % get parent name, which will be the entity name
+                parent = get_param(h,'Parent');  % get parent name with path
+                split = strsplit(parent,'/');    % split string into parts separated by '/'
+                avalon1.entity = split{end};     % get the last string
+            end
+            p=get_param(h,'CompiledPortDataTypes');
+            avalon1.avalon_sink.signal{index}.name        = get(h,'PortName');
+            avalon1.avalon_sink.signal{index}.data_type   = p.Outport{1};
+            index = index + 1;
         end
-        p=get_param(h,'CompiledPortDataTypes');
-        avalon1.avalon_sink.signal{i}.name        = get(h,'PortName');
-        avalon1.avalon_sink.signal{i}.data_type   = p.Outport{1};
-%         p=get_param(h,'runtimeObject');
-%         if isempty(p) == 0
-%             avalon1.avalon_sink.signal{i}.sample_time = p.SampleTime(1);
-%         else
-%             avalon1.avalon_sink.signal{i}.sample_time = -1;  % not sure why some have empty run time objects
-%         end
-        %avalon1.avalon_sink.signal{i}
     end
 else
     avalon1.avalon_sink_flag = 0;  % No avalon streaming sink interface
     avalon1.avalon_sink = [];
 end
+
 
 %----------------------------------
 % Get Avalon Streaming Source Info
@@ -49,21 +51,20 @@ avalon_source_signals = find_system('SearchDepth','2','regexp','on','BlockType',
 if (isempty(avalon_source_signals)==0) % The avalon streaming source exists
     avalon1.avalon_source_flag = 1;
     Na = length(avalon_source_signals);
+    index = 1;
     for i=1:Na
-        h = getSimulinkBlockHandle(avalon_source_signals{i});  % get block handle
-        p=get_param(h,'CompiledPortDataTypes');
-        avalon1.avalon_source.signal{i}.name      = get(h,'PortName');
-        avalon1.avalon_source.signal{i}.data_type = p.Inport{1};
-%         p=get_param(h,'runtimeObject');
-%         if isempty(p) == 0
-%             avalon1.avalon_source.signal{i}.sample_time = p.SampleTime(1);
-%         else
-%             avalon1.avalon_source.signal{i}.sample_time = -1;  % not sure why some have empty run time objects
-%         end
-        %avalon1.avalon_source.signal{i}
+        signal_name = avalon_source_signals{i};
+        ind = strfind(signal_name,'gm_');  % if the signal name contains gm_ we will ignore it (generated model stuff)
+        if sum(size(ind)) == 0 % signal name doesn't contain 'gm_' so process it
+            h = getSimulinkBlockHandle(signal_name);  % get block handle
+            p=get_param(h,'CompiledPortDataTypes');
+            avalon1.avalon_source.signal{index}.name      = get(h,'PortName');
+            avalon1.avalon_source.signal{index}.data_type = p.Inport{1};
+            index = index + 1;
+        end
     end
 else
-    avalon1.avalon_source_flag = 0;  % No avalon streaming source interface 
+    avalon1.avalon_source_flag = 0;  % No avalon streaming source interface
     avalon1.avalon_source = [];
 end
 
@@ -76,19 +77,24 @@ register_names = find_system('SearchDepth','2','regexp','on','BlockType','Inport
 if (isempty(register_names)==0) % The avalon memory mapped interface exists
     avalon1.avalon_memorymapped_flag = 1;
     Na = length(register_names);
+    index = 1;
     for i=1:Na
-        h = getSimulinkBlockHandle(register_names{i});  % get block handle
-        p=get_param(h,'CompiledPortDataTypes');
-        register_name = get(h,'PortName');
-        avalon1.avalon_memorymapped.register{i}.name      = register_name;
-        avalon1.avalon_memorymapped.register{i}.data_type = p.Outport{1};
-        avalon1.avalon_memorymapped.register{i}.reg_num   = i;
-        default_value = evalin('base',[register_name]);  % get value in the 'base' workspace variable
-        avalon1.avalon_memorymapped.register{i}.default_value = default_value;  
-        %avalon1.avalon_memorymapped.register{i}
+        register_name = register_names{i};
+        ind = strfind(register_name,'gm_');  % if the register name contains gm_ we will ignore it (generated model stuff)
+        if sum(size(ind)) == 0 % register name doesn't contain 'gm_' so process it
+            h = getSimulinkBlockHandle(register_name);  % get block handle
+            p=get_param(h,'CompiledPortDataTypes');
+            register_name = get(h,'PortName');
+            avalon1.avalon_memorymapped.register{index}.name      = register_name;
+            avalon1.avalon_memorymapped.register{index}.data_type = p.Outport{1};
+            avalon1.avalon_memorymapped.register{index}.reg_num   = index;
+            default_value = evalin('base',[register_name]);  % get value in the 'base' workspace variable
+            avalon1.avalon_memorymapped.register{index}.default_value = default_value;
+            index = index + 1;
+        end
     end
 else
-    avalon1.avalon_memorymapped_flag = 0;  % No avalon streaming sink interface 
+    avalon1.avalon_memorymapped_flag = 0;  % No avalon streaming sink interface
     avalon1.avalon_memorymapped = [];
 end
 
@@ -96,18 +102,23 @@ end
 % Get Exported Input Signals Info
 %----------------------------------
 conduit_names = find_system('SearchDepth','2','regexp','on','BlockType','Inport','BlockDialogParams','Export*');
-if (isempty(conduit_names)==0) 
+if (isempty(conduit_names)==0)
     avalon1.conduit_input_flag = 1;
     Na = length(conduit_names);
+    index = 1;
     for i=1:Na
-        h = getSimulinkBlockHandle(conduit_names{i});  % get block handle
-        p=get_param(h,'CompiledPortDataTypes');
-        avalon1.conduit_input.signal{i}.name      = get(h,'PortName');
-        avalon1.conduit_input.signal{i}.data_type = p.Outport{1};
-        %avalon1.conduit_input.signal{i}
+        conduit_name = conduit_names{i};
+        ind = strfind(conduit_name,'gm_');  % if the conduit name contains gm_ we will ignore it (generated model stuff)
+        if sum(size(ind)) == 0 % conduit name doesn't contain 'gm_' so process it
+            h = getSimulinkBlockHandle(conduit_name);  % get block handle
+            p=get_param(h,'CompiledPortDataTypes');
+            avalon1.conduit_input.signal{index}.name      = get(h,'PortName');
+            avalon1.conduit_input.signal{index}.data_type = p.Outport{1};
+            index = index + 1;
+        end
     end
 else
-    avalon1.conduit_input_flag = 0;  
+    avalon1.conduit_input_flag = 0;
     avalon1.conduit_input = [];
 end
 
@@ -115,18 +126,23 @@ end
 % Get Exported Output Signals Info
 %----------------------------------
 conduit_names = find_system('SearchDepth','2','regexp','on','BlockType','Outport','BlockDialogParams','Export*');
-if (isempty(conduit_names)==0) 
+if (isempty(conduit_names)==0)
     avalon1.conduit_output_flag = 1;
     Na = length(conduit_names);
+    index = 1;
     for i=1:Na
-        h = getSimulinkBlockHandle(conduit_names{i});  % get block handle
-        p=get_param(h,'CompiledPortDataTypes');
-        avalon1.conduit_output.signal{i}.name      = get(h,'PortName');
-        avalon1.conduit_output.signal{i}.data_type = p.Inport{1};
-        %avalon1.conduit_output.signal{i}
+        conduit_name = conduit_names{i};
+        ind = strfind(conduit_name,'gm_');  % if the conduit name contains gm_ we will ignore it (generated model stuff)
+        if sum(size(ind)) == 0 % conduit name doesn't contain 'gm_' so process it
+            h = getSimulinkBlockHandle(conduit_name);  % get block handle
+            p=get_param(h,'CompiledPortDataTypes');
+            avalon1.conduit_output.signal{index}.name      = get(h,'PortName');
+            avalon1.conduit_output.signal{index}.data_type = p.Inport{1};
+            index = index + 1;
+        end
     end
 else
-    avalon1.conduit_output_flag = 0;  
+    avalon1.conduit_output_flag = 0;
     avalon1.conduit_output = [];
 end
 
@@ -145,7 +161,7 @@ end
 % Turn off the compile mode 
 % Otherwise you won't be able to modify the model
 % If you can't modify, you may need to call
-% the function below multiple times (a terminate call for each compile call)
+% the function below multiple times (a terminate call is needed for each compile call)
 % because the termination has been deferred
 %------------------------------------------------
 eval([modelName,'([],[],[],''term'');']);  % terminate the compile mode
