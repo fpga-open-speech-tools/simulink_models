cbdf43084a65046452e9be21655422b10e4febf3
Ross <Ross@RossPCatFE>
2019-06-20 13:50:14 -0600

    Refactored Simple Gain model

--- simple_gain/SG_callback_stop.m
+++ simple_gain/SG_callback_stop.m
@@ -1,78 +1,34 @@
-%--------------------------------------------------------------------------
-% Description:  Matlab script to verify model ran correctly
-%               This script runs when the simulation ends (StopFcn
-%               callback found in Model Explorer)
-%--------------------------------------------------------------------------
-% Author:       Ross K. Snider
-% Company:      Flat Earth Inc
-%               985 Technology Blvd
-%               Bozeman, MT 59718
-%               support@flatearthinc.com
-% Create Date:  June 7, 2019
-% Tool Version: MATLAB R2019a
-% Revision:     1.0
-% License:      MIT License (see license at end of code)
-%--------------------------------------------------------------------------
-%------------- BEGIN CODE --------------
-%--------------------------------------------------------------------------
-% Get Avalon data
-%-------------------------------------------------------------------------
-t = Avalon_Sink_Data.Time;             % time 
-d = squeeze(Avalon_Sink_Data.Data);    % data      Note: the Matlab squeeze() function removes singleton dimensions (i.e. dimensions of length 1)
-c = squeeze(Avalon_Sink_Channel.Data); % channel
-v = squeeze(Avalon_Sink_Valid.Data);   % valid     
-left_index = 1;
-right_index = 1;
-for i=1:length(v)
-   if v(i) == 1  % check if valid, valid is asserted when there is data
-        if c(i) == 0  % if the channel number is zero, it is left channel data
-            left_data_out(left_index) = double(d(i));
-            left_time_out(left_index) = t(i);
-            left_index            = left_index + 1;
-        end
-        if c(i) == 1  % if the channel number is one, it is right channel data
-            right_data_out(right_index) = double(d(i));
-            right_time_out(right_index) = t(i);
-            right_index             = right_index + 1;
-        end
-   end
-end
+% SG_callback_stop
+%
+% This scripts captures the output signals and then verifies that
+% these signals are correct. The script runs after the simulation stops.  
+% This is called in the StopFcn callback found in Model Explorer.
+%
+% Copyright 2019 Flat Earth Inc
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+% INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+% PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+% FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+% ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+%
+% Ross K. Snider
+% Flat Earth Inc
+% 985 Technology Blvd
+% Bozeman, MT 59718
+% support@flatearthinc.com
 
-%--------------------------------------------------------------------------
-% Verify that the test data got encoded, passed through the model, and 
-% decoded correctly.  The input (modified by gain) and output values should be identical.
-%--------------------------------------------------------------------------
-left_error_max  = max(abs(left_data_in*Register_Control_Left_Gain-left_data_out));
-right_error_max = max(abs(right_data_in*Register_Control_Right_Gain-right_data_out));
-precision = 2^(-F_bits);
-% display popup message
-if (left_error_max <= precision) && (right_error_max <= precision) 
-    helpdlg(sprintf([' Max Left Error = ' num2str(left_error_max) '\n Max Right Error = ' num2str(right_error_max) '\n Error is within exceptable range \n Least significant bit precision (F_bits = ' num2str(F_bits) ') is ' num2str(2^(-F_bits))]))
-else
-    helpdlg(sprintf([' Max Left Error = ' num2str(left_error_max) '\n Max Right Error = ' num2str(right_error_max) '\n Error is NOT within exceptable range \n Least significant bit precision (F_bits = ' num2str(F_bits) ') is ' num2str(2^(-F_bits))]))
-end
+%% Put the output into the SG data struct 
+% The "To Workspace" block won't accept structs
+model_params.Avalon_Sink_Data.Time    = Avalon_Sink_Data.Time;    % time
+model_params.Avalon_Sink_Data.Data    = Avalon_Sink_Data.Data;    % data      
+model_params.Avalon_Sink_Channel.Data = Avalon_Sink_Channel.Data; % channel
+model_params.Avalon_Sink_Valid.Data   = Avalon_Sink_Valid.Data;   % valid
 
-%--------------------------------------------------------------------------
-%------------- END OF CODE --------------
-%--------------------------------------------------------------------------
-% MIT License
-% Copyright (c) 2019 Flat Earth Inc
-%
-%Permission is hereby granted, free of charge, to any person obtaining a copy
-%of this software and associated documentation files (the "Software"), to deal
-%in the Software without restriction, including without limitation the rights
-%to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-%copies of the Software, and to permit persons to whom the Software is
-%furnished to do so, subject to the following conditions:
-%
-%The above copyright notice and this permission notice shall be included in all
-%copies or substantial portions of the Software.
-%
-%THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-%IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-%FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-%AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-%LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-%OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-%SOFTWARE.
-%--------------------------------------------------------------------------
+
+if model_params.sim_prompts == 1  % sim_prompts is set in Run_me_first.m   This is turned off when the model is converted to VHDL since we don't want to run the verification multiple times at this point (HDL coder runs the simulation multiple times)
+    
+    model_params = SG_stop_process_output(model_params);  % get the output and convert from Avalon to vector
+    model_params = SG_stop_verify(model_params);          % verify that the output is correct
+    
+end
--- simple_gain/SG_hdlworkflow.m
+++ simple_gain/SG_hdlworkflow.m
@@ -1,47 +1,27 @@
 %--------------------------------------------------------------------------
-% Description:  Matlab script that converts the Simple Gain Simulink Model
-%               to VHDL.  This script was initially created by manually
-%               running the HDL Workflow Advisor on the DataPlane block
-%               and then exporting to this script.  The script was then 
-%               modified (see lines that have the % LineMod comments)
-%--------------------------------------------------------------------------
-% Author:       Ross K. Snider
-% Company:      Flat Earth Inc
-%               985 Technology Blvd
-%               Bozeman, MT 59718
-%               support@flatearthinc.com
-% Create Date:  June 12, 2019
-% Tool Version: MATLAB R2019a
-% Revision:     1.0
-% License:      MIT License (see license at end of code)
-%--------------------------------------------------------------------------
-%------------- BEGIN CODE --------------
-
-
-%--------------------------------------------------------------------------
 % HDL Workflow Script
-% Generated with MATLAB 9.6 (R2019a) at 12:03:13 on 13/06/2019
+% Generated with MATLAB 9.6 (R2019a) at 13:44:46 on 20/06/2019
 % This script was generated using the following parameter values:
-%     Filename  : 'E:\NIH\Simulink\Simple_Gain\hdlworkflow.m'
+%     Filename  : 'E:\git\nih_simulinklib\simple_gain\hdlworkflow.m'
 %     Overwrite : true
 %     Comments  : true
 %     Headers   : true
-%     DUT       : 'SG/DataPlane'
+%     DUT       : 'SG/SG_DataPlane'
 % To view changes after modifying the workflow, run the following command:
-% >> hWC.export('DUT','SG/DataPlane');
+% >> hWC.export('DUT','SG/SG_DataPlane');
 %--------------------------------------------------------------------------
 
 %% Load the Model
 load_system('SG');
 
 %% Restore the Model to default HDL parameters
-%hdlrestoreparams('SG/DataPlane');
+%hdlrestoreparams('SG/SG_DataPlane');
 
 %% Model HDL Parameters
 %% Set Model 'SG' HDL parameters
 hdlset_param('SG', 'CriticalPathEstimation', 'on');
 hdlset_param('SG', 'HDLGenerateWebview', 'on');
-hdlset_param('SG', 'HDLSubsystem', 'SG/DataPlane');
+hdlset_param('SG', 'HDLSubsystem', 'SG/SG_DataPlane');
 hdlset_param('SG', 'OptimizationReport', 'on');
 hdlset_param('SG', 'ResourceReport', 'on');
 hdlset_param('SG', 'SynthesisTool', 'Altera QUARTUS II');
@@ -49,53 +29,69 @@ hdlset_param('SG', 'SynthesisToolChipFamily', 'Cyclone V');
 hdlset_param('SG', 'SynthesisToolDeviceName', '5CSXFC6D6F31C8');
 hdlset_param('SG', 'SynthesisToolPackageName', '');
 hdlset_param('SG', 'SynthesisToolSpeedValue', '');
-hdlset_param('SG', 'TargetDirectory', [model_path '\' avalon.model_name '_' 'hdl_prj\hdlsrc']);  % LineMod
+hdlset_param('SG', 'TargetDirectory', 'hdl_prj\hdlsrc');
 hdlset_param('SG', 'TargetPlatform', 'Generic Altera Platform');
 hdlset_param('SG', 'Workflow', 'IP Core Generation');
 
 % Set SubSystem HDL parameters
-hdlset_param('SG/DataPlane', 'IPCoreName', 'DataPlane');
-hdlset_param('SG/DataPlane', 'ProcessorFPGASynchronization', 'Free running');
+hdlset_param('SG/SG_DataPlane', 'IPCoreName', 'SG_DataPlane');
+hdlset_param('SG/SG_DataPlane', 'ProcessorFPGASynchronization', 'Free running');
 
 % Set Inport HDL parameters
-hdlset_param('SG/DataPlane/Avalon_Sink_Valid', 'IOInterface', 'External Port');
-hdlset_param('SG/DataPlane/Avalon_Sink_Valid', 'IOInterfaceMapping', '');
+hdlset_param('SG/SG_DataPlane/avalon_sink_valid', 'IOInterface', 'External Port');
+hdlset_param('SG/SG_DataPlane/avalon_sink_valid', 'IOInterfaceMapping', '');
 
 % Set Inport HDL parameters
-hdlset_param('SG/DataPlane/Avalon_Sink_Data', 'IOInterface', 'External Port');
-hdlset_param('SG/DataPlane/Avalon_Sink_Data', 'IOInterfaceMapping', '');
+hdlset_param('SG/SG_DataPlane/avalon_sink_data', 'IOInterface', 'External Port');
+hdlset_param('SG/SG_DataPlane/avalon_sink_data', 'IOInterfaceMapping', '');
 
 % Set Inport HDL parameters
-hdlset_param('SG/DataPlane/Avalon_Sink_Channel', 'IOInterface', 'External Port');
-hdlset_param('SG/DataPlane/Avalon_Sink_Channel', 'IOInterfaceMapping', '');
+hdlset_param('SG/SG_DataPlane/avalon_sink_channel', 'IOInterface', 'External Port');
+hdlset_param('SG/SG_DataPlane/avalon_sink_channel', 'IOInterfaceMapping', '');
 
 % Set Inport HDL parameters
-hdlset_param('SG/DataPlane/Avalon_Sink_Error', 'IOInterface', 'External Port');
-hdlset_param('SG/DataPlane/Avalon_Sink_Error', 'IOInterfaceMapping', '');
+hdlset_param('SG/SG_DataPlane/avalon_sink_error', 'IOInterface', 'External Port');
+hdlset_param('SG/SG_DataPlane/avalon_sink_error', 'IOInterfaceMapping', '');
 
 % Set Inport HDL parameters
-hdlset_param('SG/DataPlane/Register_Control_Left_Gain', 'IOInterface', 'External Port');
-hdlset_param('SG/DataPlane/Register_Control_Left_Gain', 'IOInterfaceMapping', '');
+hdlset_param('SG/SG_DataPlane/register_control_left_gain', 'IOInterface', 'External Port');
+hdlset_param('SG/SG_DataPlane/register_control_left_gain', 'IOInterfaceMapping', '');
 
 % Set Inport HDL parameters
-hdlset_param('SG/DataPlane/Register_Control_Right_Gain', 'IOInterface', 'External Port');
-hdlset_param('SG/DataPlane/Register_Control_Right_Gain', 'IOInterfaceMapping', '');
+hdlset_param('SG/SG_DataPlane/register_control_right_gain', 'IOInterface', 'External Port');
+hdlset_param('SG/SG_DataPlane/register_control_right_gain', 'IOInterfaceMapping', '');
+
+% Set Inport HDL parameters
+hdlset_param('SG/SG_DataPlane/Avalon Data Processing/Sink_Data', 'IOInterface', 'External Port');
+hdlset_param('SG/SG_DataPlane/Avalon Data Processing/Sink_Data', 'IOInterfaceMapping', '');
+
+% Set Inport HDL parameters
+hdlset_param('SG/SG_DataPlane/Avalon Data Processing/Sink_Channel', 'IOInterface', 'External Port');
+hdlset_param('SG/SG_DataPlane/Avalon Data Processing/Sink_Channel', 'IOInterfaceMapping', '');
 
 % Set Outport HDL parameters
-hdlset_param('SG/DataPlane/Avalon_Source_Valid', 'IOInterface', 'External Port');
-hdlset_param('SG/DataPlane/Avalon_Source_Valid', 'IOInterfaceMapping', '');
+hdlset_param('SG/SG_DataPlane/Avalon Data Processing/Source_Data', 'IOInterface', 'External Port');
+hdlset_param('SG/SG_DataPlane/Avalon Data Processing/Source_Data', 'IOInterfaceMapping', '');
 
 % Set Outport HDL parameters
-hdlset_param('SG/DataPlane/Avalon_Source_Data', 'IOInterface', 'External Port');
-hdlset_param('SG/DataPlane/Avalon_Source_Data', 'IOInterfaceMapping', '');
+hdlset_param('SG/SG_DataPlane/Avalon Data Processing/Source_Channel', 'IOInterface', 'External Port');
+hdlset_param('SG/SG_DataPlane/Avalon Data Processing/Source_Channel', 'IOInterfaceMapping', '');
 
 % Set Outport HDL parameters
-hdlset_param('SG/DataPlane/Avalon_Source_Channel', 'IOInterface', 'External Port');
-hdlset_param('SG/DataPlane/Avalon_Source_Channel', 'IOInterfaceMapping', '');
+hdlset_param('SG/SG_DataPlane/avalon_source_valid', 'IOInterface', 'External Port');
+hdlset_param('SG/SG_DataPlane/avalon_source_valid', 'IOInterfaceMapping', '');
 
 % Set Outport HDL parameters
-hdlset_param('SG/DataPlane/Avalon_Source_Error', 'IOInterface', 'External Port');
-hdlset_param('SG/DataPlane/Avalon_Source_Error', 'IOInterfaceMapping', '');
+hdlset_param('SG/SG_DataPlane/avalon_source_data', 'IOInterface', 'External Port');
+hdlset_param('SG/SG_DataPlane/avalon_source_data', 'IOInterfaceMapping', '');
+
+% Set Outport HDL parameters
+hdlset_param('SG/SG_DataPlane/avalon_source_channel', 'IOInterface', 'External Port');
+hdlset_param('SG/SG_DataPlane/avalon_source_channel', 'IOInterfaceMapping', '');
+
+% Set Outport HDL parameters
+hdlset_param('SG/SG_DataPlane/avalon_source_error', 'IOInterface', 'External Port');
+hdlset_param('SG/SG_DataPlane/avalon_source_error', 'IOInterfaceMapping', '');
 
 
 %% Workflow Configuration Settings
@@ -103,7 +99,7 @@ hdlset_param('SG/DataPlane/Avalon_Source_Error', 'IOInterfaceMapping', '');
 hWC = hdlcoder.WorkflowConfig('SynthesisTool','Altera QUARTUS II','TargetWorkflow','IP Core Generation');
 
 % Specify the top level project directory
-hWC.ProjectFolder = [avalon.model_name '_' 'hdl_prj'];   % LineMod
+hWC.ProjectFolder = 'hdl_prj';
 hWC.ReferenceDesignToolVersion = '';
 hWC.IgnoreToolVersionMismatch = false;
 
@@ -137,30 +133,4 @@ hWC.ProgrammingMethod = hdlcoder.ProgrammingMethod.JTAG;
 hWC.validate;
 
 %% Run the workflow
-hdlcoder.runWorkflow('SG/DataPlane', hWC);
-
-%--------------------------------------------------------------------------
-%------------- END OF CODE --------------
-%--------------------------------------------------------------------------
-% MIT License
-% Copyright (c) 2019 Flat Earth Inc
-%
-%Permission is hereby granted, free of charge, to any person obtaining a copy
-%of this software and associated documentation files (the "Software"), to deal
-%in the Software without restriction, including without limitation the rights
-%to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-%copies of the Software, and to permit persons to whom the Software is
-%furnished to do so, subject to the following conditions:
-%
-%The above copyright notice and this permission notice shall be included in all
-%copies or substantial portions of the Software.
-%
-%THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-%IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-%FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-%AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-%LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-%OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-%SOFTWARE.
-%--------------------------------------------------------------------------
-
+hdlcoder.runWorkflow('SG/SG_DataPlane', hWC);
