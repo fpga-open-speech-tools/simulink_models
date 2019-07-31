00a4e7b1e3f140ad3bf76f2836698cf4a8c2cb07
Ross <snider@flatearthinc.com>
2019-07-31 12:38:45 -0600

    Change the model to have a system clock of Mclk*8 = 12.288MHz*8=98304000 that Qsys expects  (it was 49152000)

--- models/simple_gain/sm_callback_init.m
+++ models/simple_gain/sm_callback_init.m
@@ -31,7 +31,7 @@ mp.Ts = 1/mp.Fs;  % sample period
 %% Set the FPGA system clock frequency (frequency of the FPGA fabric)
 % The system clock frequency should be an integer multiple of the Audio codec AD1939 Mclk frequency (12.288 MHz)
 if mp.fastsim_flag == 0
-    mp.Fs_system = 49152000;        % System clock frequency in Hz of Avalon Interface  Mclk*4 = 12.288MHz*4=49152000
+    mp.Fs_system = 98304000;        % System clock frequency in Hz of Avalon Interface  Mclk*8 = 12.288MHz*8=98304000
 else
     mp.Fs_system = mp.Fs * mp.fastsim_Fs_system_N;          % Note: For faster development runs (faster sim times), reduce the number of system clocks between samples.  mp.fastsim_Fs_system_N is set in sm_run_me_first.m
 end
