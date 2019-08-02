5989de44d0634353cf63c2311c7f424fc1b07eef
Trevor Vannoy <vannoy@flatearthinc.com>
2019-08-02 11:24:47 -0600

    fixed case-sensitvity issue that caused sg_dataplane.vhd to still be added to the tcl script

--- utilities/vhdl_codegen/populate_additional_filesets.py
+++ utilities/vhdl_codegen/populate_additional_filesets.py
@@ -2,6 +2,6 @@ import os
 def populate_additional_filesets(input_struct, additionalFilesetAbsDir):
     for filename in os.listdir(additionalFilesetAbsDir):
         if filename.endswith(".vhd") and not "axi4" in filename and not "_avalon" in filename:
-            if filename != input_struct.model_abbreviation + "_dataplane.vhd":
+            if filename.lower() != (input_struct.model_abbreviation + "_dataplane.vhd").lower():
                 input_struct.additional_filesets.append(filename)
                 print("added file " + filename)
