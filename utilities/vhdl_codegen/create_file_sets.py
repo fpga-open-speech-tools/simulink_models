dc4e89c04b35d8ad690944cd3be9c8efafcea66b
Trevor Vannoy <vannoy@flatearthinc.com>
2019-08-02 12:06:49 -0600

    add the top level file last so quartus can succesfilly compile

--- utilities/vhdl_codegen/create_file_sets.py
+++ utilities/vhdl_codegen/create_file_sets.py
@@ -3,17 +3,21 @@ def create_file_sets(input_struct):
     built_string += "# created in create_file_sets\n"
     built_string += "# # # # # # # # # # # # # # # # # #\n\n"
     built_string += "add_fileset QUARTUS_SYNTH QUARTUS_SYNTH \"\" \"\"\n"
+    
+    # add additional dependent files before the top level file; quartus needs to compile dependencies first
+    for i in range(len(input_struct.additional_filesets)):
+        built_string += ("add_fileset_file " + input_struct.additional_filesets[i].ljust(60, ' ') + " VHDL PATH " +
+                     "hdl/" + input_struct.additional_filesets[i] + "\n")
+
+    # add the top level file
     built_string += "set_fileset_property QUARTUS_SYNTH TOP_LEVEL " + input_struct.quartus_synth_top_level + "\n"
     built_string += ("set_fileset_property QUARTUS_SYNTH ENABLE_RELATIVE_INCLUDE_PATHS " +
                      str(input_struct.enable_rel_inc_paths).lower() + "\n")
     built_string += ("set_fileset_property QUARTUS_SYNTH ENABLE_FILE_OVERWRITE_MODE " +
                      str(input_struct.enable_file_overwrite).lower() + "\n")
-    built_string += ("add_fileset_file " + input_struct.vhdl_top_level_file.ljust(60) + " VHDL PATH hdl/" +
                      input_struct.vhdl_top_level_file + " TOP_LEVEL_FILE\n")
-    for i in range(len(input_struct.additional_filesets)):
-        built_string += ("add_fileset_file " + input_struct.additional_filesets[i].ljust(60, ' ') + " VHDL PATH " +
-                     "hdl/" + input_struct.additional_filesets[i] + "\n")
     built_string += "# end create_file_sets\n\n\n"
+
     return built_string
 
 
