3f13374623c7a3b81925e3b1b38812bba151f700
Ross <ross.snider@montana.edu>
2019-09-24 13:11:38 -0600

    initial commit
    initial commit

---
+++ models/bitcrusher/bit_mask_create.m
@@ -0,0 +1,95 @@
+
+% Note the HDL coder doesn't allow writing properties to embedded.fi
+% so we will create the associated values for the bit bitmask and
+% write the Matlab code that does this and that can be pasted into
+% the user matlab function block
+
+fid = fopen('bit_mask_code.txt','w');
+
+
+sline = ['function bitmask = getbitmask(bitlevel)']; fprintf(fid,'%s\n',sline);
+sline = ['bitmask=fi(0,1,32,28);']; fprintf(fid,'%s\n',sline);
+sline = ['switch(bitlevel)']; fprintf(fid,'%s\n',sline);
+
+bitmask=fi(0,1,32,28);
+for i=0:32
+    switch(i)
+        case 32
+            bitmask.bin = '11111111111111111111111111111111';
+        case 31
+            bitmask.bin = '11111111111111111111111111111110';
+        case 30
+            bitmask.bin = '11111111111111111111111111111100';
+        case 29
+            bitmask.bin = '11111111111111111111111111111000';
+        case 28
+            bitmask.bin = '11111111111111111111111111110000';
+        case 27
+            bitmask.bin = '11111111111111111111111111100000';
+        case 26
+            bitmask.bin = '11111111111111111111111111000000';
+        case 25
+            bitmask.bin = '11111111111111111111111110000000';
+        case 24
+            bitmask.bin = '11111111111111111111111100000000';
+        case 23
+            bitmask.bin = '11111111111111111111111000000000';
+        case 22
+            bitmask.bin = '11111111111111111111110000000000';
+        case 21
+            bitmask.bin = '11111111111111111111100000000000';
+        case 20
+            bitmask.bin = '11111111111111111111000000000000';
+        case 19
+            bitmask.bin = '11111111111111111110000000000000';
+        case 18
+            bitmask.bin = '11111111111111111100000000000000';
+        case 17
+            bitmask.bin = '11111111111111111000000000000000';
+        case 16
+            bitmask.bin = '11111111111111110000000000000000';
+        case 15
+            bitmask.bin = '11111111111111100000000000000000';
+        case 14
+            bitmask.bin = '11111111111111000000000000000000';
+        case 13
+            bitmask.bin = '11111111111110000000000000000000';
+        case 12
+            bitmask.bin = '11111111111100000000000000000000';
+        case 11
+            bitmask.bin = '11111111111000000000000000000000';
+        case 10
+            bitmask.bin = '11111111110000000000000000000000';
+        case 9
+            bitmask.bin = '11111111100000000000000000000000';
+        case 8
+            bitmask.bin = '11111111000000000000000000000000';
+        case 7
+            bitmask.bin = '11111110000000000000000000000000';
+        case 6
+            bitmask.bin = '11111100000000000000000000000000';
+        case 5
+            bitmask.bin = '11111000000000000000000000000000';
+        case 4
+            bitmask.bin = '11110000000000000000000000000000';
+        case 3
+            bitmask.bin = '11100000000000000000000000000000';
+        case 2
+            bitmask.bin = '11000000000000000000000000000000';
+        case 1
+            bitmask.bin = '10000000000000000000000000000000';
+        case 0
+            bitmask.bin = '00000000000000000000000000000000';
+    end
+    
+    sline = [blanks(4) 'case ' num2str(i)]; fprintf(fid,'%s\n',sline);
+    %sline = [blanks(8) 'bitmask = ' num2str(bitmask,'%10d') ';']; fprintf(fid,'%s\n',sline);
+    sline = [blanks(8) 'bitmask = fi(' num2str(bitmask,'%4.28f') ',1,32,28);']; fprintf(fid,'%s\n',sline);
+    
+    %eval(['b = fi(' num2str(bitmask,'%4.28f') ',1,32,28);']);
+    %b.bin
+   
+end
+sline = ['end']; fprintf(fid,'%s\n',sline);
+
+
---
+++ models/bitcrusher/sm_init_control_signals.m
@@ -0,0 +1,63 @@
+function mp = sm_init_control_signals(mp)
+% mp = sm_init_control_signals(mp)
+%
+% Matlab function that creates and initializes the control signals. 
+% The min/max values the control signals are expected to take need to be 
+% defined.
+%
+% Inputs:
+%   mp, which is the model data structure that holds the model parameters
+%
+% Outputs:
+%   mp, the model data structure that now contains the registers in the 
+%   field mp.register(i), indexed by i, which has the following fields:
+%         mp.register(i).name       -  The register name
+%         mp.register(i).value      -  The value the control register is currently set to
+%         mp.register(i).min        -  The minimum value the control signal will ever take
+%         mp.register(i).max        -  The maximum value the control signal will ever take
+%         mp.register(i).timeseries - The timeseries data format that is needed for the from workspace block
+%
+% Copyright 2019 Flat Earth Inc
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+% INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+% PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+% FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+% ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+%
+% Ross K. Snider
+% Flat Earth Inc
+% 985 Technology Blvd
+% Bozeman, MT 59718
+% support@flatearthinc.com
+
+
+%% Create left gain control signal
+mp.register(1).name       = 'Left_Bits';  % control signal name
+mp.register(1).value      = 20;          % value control signal will take during simulation
+mp.register(1).min        =  0;          % The minimum value the control signal will ever take
+mp.register(1).max        = 32;          % The maximum value the control signal will ever take
+mp.register(1).default    = 32;          % default (initial) value
+mp.register(1).widget_type          = 'slider';
+mp.register(1).widget_display_units = 'bits';
+
+%% Create right gain control signal
+mp.register(2).name       = 'Right_Bits';  % control signal name
+mp.register(2).value      = 9;          % value control signal will take during simulation
+mp.register(2).min        =  0;          % The minimum value the control signal will ever take
+mp.register(2).max        = 32;          % The maximum value the control signal will ever take
+mp.register(2).default    = 32;          % default (initial) value
+mp.register(2).widget_type          = 'slider';
+mp.register(2).widget_display_units = 'bits';
+
+% Any other register control signals should be created in a similar manner
+
+
+%% convert to time series data
+for i=1:length(mp.register)
+    mp.register(i).timeseries = timeseries(mp.register(i).value,0);  % timeseries(datavals,timevals);  % A timeseries data format is needed for the from workspace block     
+end
+
+%% Check if the control signals have valid entries
+sm_check_control_signals(mp)
+
