26ea60e15f533a82f70aa8155d610c131758c7ac
unknown <bailey0522@gmail.com>
2020-01-17 17:16:46 -0700

    trying to figure out variable propogation under a mask

--- models/Dynamic_Compression_Model/slblocks.m
+++ models/Dynamic_Compression_Model/slblocks.m
@@ -1,4 +1,4 @@
-function blkStruct = 
+function blkStruct = slblocks
 		% This function specifies that the library should appear
 		% in the Library Browser
 		% and be cached in the browser repository
