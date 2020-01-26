e596e066480fe8c42d22d20e4460b8b923022afa
Bailey Galacci <bailey0522@gmail.com>
2020-01-26 14:13:02 -0700

    1 26

--- models/Dynamic_Compression_Model/testingTEMP.m
+++ models/Dynamic_Compression_Model/testingTEMP.m
@@ -5,3 +5,12 @@ tt = tt./48000;
 
 simin = [tt',x_in'];
 stop_time = tt(end);
+
+address(:,1) = simout{2}.Values.data(:);
+address(2:1001,1) = address(1:1000,1);
+address(:,1) = abs(31-address(:,1));
+address_r = address(:,1).*2+address(:,2);
+figure(); semilogx(tt,address_r);
+title("Read address over time");
+xlabel("Time");
+ylabel("Read Address");
