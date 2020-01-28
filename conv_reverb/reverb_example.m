bf16107edd585b7b9481363f217cd181748a9681
Alex Salois <asalois.scholar@gmail.com>
2020-01-28 14:21:40 -0700

    print the response

--- conv_reverb/reverb_example.m
+++ conv_reverb/reverb_example.m
@@ -21,16 +21,34 @@ audiowrite('room_reverb.wav', y, Fs);
 % box
 y = fconv(x,imp_box);
 audiowrite('box_reverb.wav', y, Fs);
+figure();
+plot(imp_box);
+title('Box Impulse Response')
+print('box.png', '-dpng')
 
 % cave
 y = fconv(x,imp_cave);
 audiowrite('cave_reverb.wav', y, Fs);
+figure();
+plot(imp_cave);
+title('Cave Impulse Response')
+print('cave.png', '-dpng')
+
 
 % hall
 y = fconv(x,imp_hall);
 audiowrite('hall_reverb.wav', y, Fs);
+figure();
+plot(imp_hall);
+title('Big Hall Impulse Response')
+print('hall.png', '-dpng')
 
 % pipe 
 y = fconv(x,imp_pipe);
 audiowrite('pipe_reverb.wav', y, Fs);
+figure();
+plot(imp_pipe);
+title('Pipe Impulse Response')
+print('pipe.png', '-dpng')
+
 
