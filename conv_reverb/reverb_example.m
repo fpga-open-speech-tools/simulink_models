a7d08c88f4d45ca945c0aec92928ad098bdbf845
Alex Salois <asalois.scholar@gmail.com>
2020-01-28 11:10:22 -0700

    added impulse responses

--- conv_reverb/reverb_example.m
+++ conv_reverb/reverb_example.m
@@ -5,11 +5,32 @@
 [x,Fs] = audioread('acoustic.wav'); 
 
 % read the impulse response waveform
-%filename = 'impulse_room.wav'; 
-[imp,Fsimp] = audioread('impulse_room.wav');
+[imp_room, Fsimp] = audioread('impulse_room.wav');
+[imp_box, Fsimp] = audioread('STRANGEBOX-2.wav');
+[imp_cave, Fsimp] = audioread('DAMPED CAVE E001 M2S.wav');
+[imp_hall, Fsimp] = audioread('BIG HALL E001 M2S.wav');
+[imp_pipe, Fsimp] = audioread('PIPE & CARPET E001 M2S.wav');
 
 % Do convolution with FFT
-y = fconv(x,imp);
+y = fconv(x,imp_room);
 
 % write output
-audiowrite('out_IRreverb.wav', y, Fs)
+audiowrite('room_reverb.wav', y, Fs);
+
+
+% box
+y = fconv(x,imp_box);
+audiowrite('box_reverb.wav', y, Fs);
+
+% cave
+y = fconv(x,imp_cave);
+audiowrite('cave_reverb.wav', y, Fs);
+
+% hall
+y = fconv(x,imp_hall);
+audiowrite('hall_reverb.wav', y, Fs);
+
+% pipe 
+y = fconv(x,imp_pipe);
+audiowrite('pipe_reverb.wav', y, Fs);
+
