7815c21af28886db3da6c7fd3d15db011037d8db
Alex Salois <asalois.scholar@gmail.com>
2020-02-18 16:58:46 -0700

    copy an pasted to get fiaccel to work with different bit depths

--- conv_reverb/reverb_example.m
+++ conv_reverb/reverb_example.m
@@ -2,6 +2,7 @@
 % Script to call implement Convolution Reverb
 % read the sample waveform
 % filename = 'acoustic.wav';
+
 [x,Fs] = audioread('acoustic.wav'); 
 
 % read the impulse response waveform
@@ -10,50 +11,124 @@
 [imp_cave, Fsimp] = audioread('DAMPED CAVE E001 M2S.wav');
 [imp_hall, Fsimp] = audioread('BIG HALL E001 M2S.wav');
 [imp_pipe, Fsimp] = audioread('PIPE & CARPET E001 M2S.wav');
-
-
-% write output
-% audiowrite('room_reverb.wav', y, Fs);
-
-w = 16;
-f = w - 2;
-
-h = imp_cave(:,1); % impulse response
-
-% define fixed point
-Fm = fimath('RoundingMethod','Floor',...
-        'OverflowAction','Wrap',...
-        'ProductMode','SpecifyPrecision',...
-        'ProductWordLength',w,...
-        'ProductFractionLength',f,...
-        'SumMode','SpecifyPrecision',...
-        'SumWordLength',w,...
-        'SumFractionLength',f);
-
-
-disp('c compile');
 tic
-argsIn = {x,h};
-fiaccel conv_fixedpt... % function
-    -args argsIn... % number and argumanets in
-    -nargout 1 % number of outputs
-toc
+i = 32
+name = ['room_reverb_w_' int2str(i) '.wav'] % name of the file
 
-disp('Convolution w/ FFT and w/o fixed points');
-tic
-y = fconv(x,h);
-toc
+h = imp_room(:,1); % impulse response
 
-disp('Convolution w/o fixed points');
-tic 
-y_conv = conv(x,h);
-toc
+% compile the c code
+argsIn = {x, h};
+fiaccel  conv_fixedpt_32... % function
+        -args argsIn... % number and argumanets in
+        -nargout 1  % number of outputs
 
-disp('Convolution w fixed points and c');
-tic
+% Convolution w fixed points and c
 y_fi = conv_fixedpt_mex(x, h); 
-toc
+    
+% normalize and type cast
+y = double(y_fi/max(abs(y_fi)));
+    audiowrite(name, y, Fs);
+  
+i = 28    
+name = ['room_reverb_w_' int2str(i) '.wav'] % name of the file
 
+% compile the c code
+argsIn = {x, h};
+fiaccel  conv_fixedpt_28... % function
+        -args argsIn... % number and argumanets in
+        -nargout 1  % number of outputs
+
+% Convolution w fixed points and c
+y_fi = conv_fixedpt_mex(x, h); 
+    
+% normalize and type cast
+y = double(y_fi/max(abs(y_fi)));
+    audiowrite(name, y, Fs);
+    
+i = 24    
+name = ['room_reverb_w_' int2str(i) '.wav'] % name of the file
+
+% compile the c code
+argsIn = {x, h};
+fiaccel  conv_fixedpt_24... % function
+        -args argsIn... % number and argumanets in
+        -nargout 1  % number of outputs
+
+% Convolution w fixed points and c
+y_fi = conv_fixedpt_mex(x, h); 
+    
+% normalize and type cast
+y = double(y_fi/max(abs(y_fi)));
+    audiowrite(name, y, Fs);
+
+i = 20    
+name = ['room_reverb_w_' int2str(i) '.wav'] % name of the file
+
+% compile the c code
+argsIn = {x, h};
+fiaccel  conv_fixedpt_20... % function
+        -args argsIn... % number and argumanets in
+        -nargout 1  % number of outputs
+
+% Convolution w fixed points and c
+y_fi = conv_fixedpt_mex(x, h); 
+    
+% normalize and type cast
+y = double(y_fi/max(abs(y_fi)));
+    audiowrite(name, y, Fs);
+    
+i = 16   
+name = ['room_reverb_w_' int2str(i) '.wav'] % name of the file
+
+% compile the c code
+argsIn = {x, h};
+fiaccel  conv_fixedpt_16... % function
+        -args argsIn... % number and argumanets in
+        -nargout 1  % number of outputs
+
+% Convolution w fixed points and c
+y_fi = conv_fixedpt_mex(x, h); 
+    
+% normalize and type cast
+y = double(y_fi/max(abs(y_fi)));
+    audiowrite(name, y, Fs);
+
+i = 12
+name = ['room_reverb_w_' int2str(i) '.wav'] % name of the file
+
+% compile the c code
+argsIn = {x, h};
+fiaccel  conv_fixedpt_12... % function
+        -args argsIn... % number and argumanets in
+        -nargout 1  % number of outputs
+
+% Convolution w fixed points and c
+y_fi = conv_fixedpt_mex(x, h); 
+    
+% normalize and type cast
+y = double(y_fi/max(abs(y_fi)));
+    audiowrite(name, y, Fs);
+
+i = 8   
+name = ['room_reverb_w_0' int2str(i) '.wav'] % name of the file
+
+% compile the c code
+argsIn = {x, h};
+fiaccel  conv_fixedpt_08... % function
+        -args argsIn... % number and argumanets in
+        -nargout 1  % number of outputs
+
+% Convolution w fixed points and c
+y_fi = conv_fixedpt_mex(x, h); 
+    
+% normalize and type cast
+y = double(y_fi/max(abs(y_fi)));
+    audiowrite(name, y, Fs);
+
+
+    
+toc
 
 %% do not do unless you have a lot of time
 % tic
