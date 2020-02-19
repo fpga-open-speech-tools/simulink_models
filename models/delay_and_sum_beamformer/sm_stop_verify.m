07d33af258a87f113f1e1d6496a4b326c7ff36ef
Trevor Vannoy <vannoy@flatearthinc.com>
2020-02-19 11:25:31 -0700

    create model and directory for delay and sum beamformer

---
+++ models/delay_and_sum_beamformer/sm_stop_verify.m
@@ -0,0 +1,35 @@
+% mp = sm_stop_verify(mp)
+%
+% Matlab function that verifies the model output 
+
+% Inputs:
+%   mp, which is the model data structure that holds the model parameters
+%
+% Outputs:
+%   mp, the model data structure that now contains the left/right channel
+%   data, which is in the following format:
+%          mp.left_data_out         - The processed left channel data
+%          mp.left_time_out         - time of left channel data
+%          mp.right_data_out        - The processed right channel data
+%          mp.right_time_out        - time of right channel data
+%
+% Copyright 2019 Flat Earth Inc
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+% INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+% PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+% FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+% ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+%
+% Ross K. Snider
+% Flat Earth Inc
+% 985 Technology Blvd
+% Bozeman, MT 59718
+% support@flatearthinc.com
+
+function mp = sm_stop_verify(mp)
+
+%% Verify that the test data got encoded, passed through the model, and
+% decoded correctly.  The input (modified by gain) and output values should be identical.
+
+end
