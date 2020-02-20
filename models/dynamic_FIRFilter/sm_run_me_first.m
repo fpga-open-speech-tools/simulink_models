504fd114edb37f7474f0814a074a7a818a713b88
Justin <justin.williams15@student.montana.edu>
2020-02-19 21:21:33 -0700

    Ran the code again to verify outputs. Generated HDL.

--- models/dynamic_FIRFilter/sm_run_me_first.m
+++ models/dynamic_FIRFilter/sm_run_me_first.m
@@ -41,6 +41,7 @@ mp.fastsim_Nsamples    = 12000; % set to the string 'all' to use all the samples
 % Model parameters are placed in a data structure called mp that can be passed to functions
 mp.model_name = 'pFIR_HPF_testing_and_analysis';
 mp.model_abbreviation = 'pFIR_HPF';
+mp.model_abbreviation = 'pFIR_HPF_testing_and_analysis';
 
 % Device driver version for the Linux device tree. Typically set as the Quartus version
 mp.linux_device_name = mp.model_name;
