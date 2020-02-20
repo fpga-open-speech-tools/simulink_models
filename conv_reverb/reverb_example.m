1df0de680f1184bdd06ac4186754bcab7fcc3779
Alex Salois <asalois.scholar@gmail.com>
2020-02-20 08:11:23 -0700

    all the input files have thier own loop to generate wav files

--- conv_reverb/reverb_example.m
+++ conv_reverb/reverb_example.m
@@ -11,13 +11,145 @@
 [imp_cave, Fsimp] = audioread('DAMPED CAVE E001 M2S.wav');
 [imp_hall, Fsimp] = audioread('BIG HALL E001 M2S.wav');
 [imp_pipe, Fsimp] = audioread('PIPE & CARPET E001 M2S.wav');
-h = imp_cave(:,1); % impulse response
 
+%%
 tic
+disp('cave')
+h = imp_cave(:,1); % impulse response
 for i = 32:-1:10
-    width = i
+    width = i;
     frac = width - 8;
-    name = ['cave_reverb_w_' int2str(i) '.wav'] % name of the file
+    name = ['cave_reverb_w_' int2str(i) '.wav']; % name of the file
+    Fm = fimath('RoundingMethod','Floor',...
+        'OverflowAction','Wrap',...
+        'ProductMode','SpecifyPrecision',...
+        'ProductWordLength',width,...
+        'ProductFractionLength',frac,...
+        'SumMode','SpecifyPrecision',...
+        'SumWordLength',width,...
+        'SumFractionLength',frac);
+    
+    x_fi = fi(x, 1, width, frac, Fm);
+    h_fi = fi(h, 1, width, frac, Fm);
+
+    argsIn = {x_fi, h_fi};
+    fiaccel  fi_conv... % function
+            -args argsIn... % number and argumanets in
+            -nargout 1  % number of outputs
+        
+    y_fi = fi_conv_mex(x_fi, h_fi);
+    y = double(y_fi);
+    y = y ./ max(abs(y));
+    audiowrite(name, y, Fs);
+end
+toc
+
+%%
+tic
+disp('room')
+h = imp_room(:,1); % impulse response
+for i = 32:-1:8
+    width = i;
+    frac = width - 2;
+    name = ['room_reverb_w_' int2str(i) '.wav']; % name of the file
+    Fm = fimath('RoundingMethod','Floor',...
+        'OverflowAction','Wrap',...
+        'ProductMode','SpecifyPrecision',...
+        'ProductWordLength',width,...
+        'ProductFractionLength',frac,...
+        'SumMode','SpecifyPrecision',...
+        'SumWordLength',width,...
+        'SumFractionLength',frac);
+    
+    x_fi = fi(x, 1, width, frac, Fm);
+    h_fi = fi(h, 1, width, frac, Fm);
+
+    argsIn = {x_fi, h_fi};
+    fiaccel  fi_conv... % function
+            -args argsIn... % number and argumanets in
+            -nargout 1  % number of outputs
+        
+    y_fi = fi_conv_mex(x_fi, h_fi);
+    y = double(y_fi);
+    y = y ./ max(abs(y));
+    audiowrite(name, y, Fs);
+end
+toc
+
+
+%%
+tic
+disp('box')
+h = imp_box(:,1); % impulse response
+for i = 32:-1:8
+    width = i;
+    frac = width - 7;
+    name = ['box_reverb_w_' int2str(i) '.wav']; % name of the file
+    Fm = fimath('RoundingMethod','Floor',...
+        'OverflowAction','Wrap',...
+        'ProductMode','SpecifyPrecision',...
+        'ProductWordLength',width,...
+        'ProductFractionLength',frac,...
+        'SumMode','SpecifyPrecision',...
+        'SumWordLength',width,...
+        'SumFractionLength',frac);
+    
+    x_fi = fi(x, 1, width, frac, Fm);
+    h_fi = fi(h, 1, width, frac, Fm);
+
+    argsIn = {x_fi, h_fi};
+    fiaccel  fi_conv... % function
+            -args argsIn... % number and argumanets in
+            -nargout 1  % number of outputs
+        
+    y_fi = fi_conv_mex(x_fi, h_fi);
+    y = double(y_fi);
+    y = y ./ max(abs(y));
+    audiowrite(name, y, Fs);
+end
+toc
+
+
+%%
+tic
+disp('pipe')
+h = imp_pipe(:,1); % impulse response
+for i = 32:-1:8
+    width = i;
+    frac = width - 7;
+    name = ['pipe_reverb_w_' int2str(i) '.wav']; % name of the file
+    Fm = fimath('RoundingMethod','Floor',...
+        'OverflowAction','Wrap',...
+        'ProductMode','SpecifyPrecision',...
+        'ProductWordLength',width,...
+        'ProductFractionLength',frac,...
+        'SumMode','SpecifyPrecision',...
+        'SumWordLength',width,...
+        'SumFractionLength',frac);
+    
+    x_fi = fi(x, 1, width, frac, Fm);
+    h_fi = fi(h, 1, width, frac, Fm);
+
+    argsIn = {x_fi, h_fi};
+    fiaccel  fi_conv... % function
+            -args argsIn... % number and argumanets in
+            -nargout 1  % number of outputs
+        
+    y_fi = fi_conv_mex(x_fi, h_fi);
+    y = double(y_fi);
+    y = y ./ max(abs(y));
+    audiowrite(name, y, Fs);
+end
+toc
+
+%%
+tic
+disp('hall')
+h = imp_hall(:,1); % impulse response
+for i = 32:-1:8
+    width = i;
+    frac = width - 7;
+    name = ['hall_reverb_w_' int2str(i) '.wav']; % name of the file
     Fm = fimath('RoundingMethod','Floor',...
         'OverflowAction','Wrap',...
         'ProductMode','SpecifyPrecision',...
