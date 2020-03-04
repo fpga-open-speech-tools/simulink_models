30e2b14ff8a8bf3e4a06363409344aa9a1bafaad
matthewblunt <55067306+matthewblunt@users.noreply.github.com>
2020-03-03 17:17:28 -0700

    Finished Spike Generator Double to Single Precision Test/Verification and Edited Stateflow Function for Full AN Simulink Model

--- models/AN_Simulink_Model_v1_Singles/AN_Subfunction_Verification/AN_Spike_Generator/Verify_Spike_Generator.m
+++ models/AN_Simulink_Model_v1_Singles/AN_Subfunction_Verification/AN_Spike_Generator/Verify_Spike_Generator.m
@@ -24,23 +24,79 @@
 % the results are not fully verified, only tested.
 
 
-%% OUTPUT PLOT
- 
+%% OUTPUT COMPARISON PLOTS
+% *** Changed to include Double and Single Comparison Plots
+% *** By Matthew Blunt 03/03/2020
+
 figure()
-subplot(3,1,1)
-stem(out.sptime);
-title('Spike Train Output (sptime) - Simulink Model');
+subplot(2,1,1)
+stem(out.sptimedouble);
+title('Spike Train Output (sptime) - Simulink Double Precision');
 ylabel('Spike Indicator');
 xlabel('Sample Number');
-subplot(3,1,3)
-plot(out.trd_vector);
-title('Mean Synaptic Redocking Time');
+subplot(2,1,2)
+stem(out.sptime);
+title('Spike Train Output (sptime) - Simulink Single Precision');
+ylabel('Spike Indicator');
+xlabel('Sample Number');
+
+figure()
+subplot(2,1,1)
+plot(out.trd_vectordouble);
+title('Mean Synaptic Redocking Time - Simulink Double Precision');
 ylabel('Redocking Time [sec]');
 xlabel('Sample Number');
-subplot(3,1,2)
-plot(out.meanrate);
-title('Mean of Spike Rate');
+subplot(2,1,2)
+plot(out.trd_vector);
+title('Mean Synaptic Redocking Time - Simulink Single Precision');
+ylabel('Redocking Time [sec]');
+xlabel('Sample Number');
+
+figure()
+subplot(2,1,1)
+plot(out.meanratedouble);
+title('Mean of Spike Rate - Simulink Double Precision');
 ylabel('Mean Rate [/s]');
 xlabel('Sample Number');
+subplot(2,1,2)
+plot(out.meanrate);
+title('Mean of Spike Rate - Simulink Single Precision');
+ylabel('Mean Rate [/s]');
+xlabel('Sample Number');
+
+
+%% ERROR CALCULATION
+% *** Added to include Double vs. Single Error Calculations
+% *** By Matthew Blunt 03/03/2020
+
+% Compares output vectors of Simulink Double and Single Precision Spike
+% Time Outputs
+% *** Added by Matthew Blunt 03/03/2020
+sptimeconversionerror = norm(out.sptimedouble(1:end)-out.sptime(1:end));
+
+% Display in Simulink Diagnostics menu
+% *** Added by Matthew Blunt 03/03/2020
+disp('Spike Time Conversion Error =');
+disp(sptimeconversionerror);
+
+% Compares output vectors of Simulink Double and Single Precision Redocking
+% Time Outputs
+% *** Added by Matthew Blunt 03/03/2020
+trdconversionerror = norm(out.trd_vectordouble(1:end)-out.trd_vector(1:end));
+
+% Display in Simulink Diagnostics menu
+% *** Added by Matthew Blunt 03/03/2020
+disp('Redocking Time Conversion Error =');
+disp(trdconversionerror);
+
+% Compares output vectors of Simulink Double and Single Precision Mean
+% Synaptic Rate Outputs
+% *** Added by Matthew Blunt 03/03/2020
+meanrateconversionerror = norm(out.meanratedouble(1:end)-out.meanrate(1:end));
+
+% Display in Simulink Diagnostics menu
+% *** Added by Matthew Blunt 03/03/2020
+disp('Mean Spike Rate Conversion Error =');
+disp(meanrateconversionerror);
 
 % end of script
