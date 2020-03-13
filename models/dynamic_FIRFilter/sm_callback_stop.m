0d169da7cfebdc40ad79b5e09fe07143562d3746
Bailey Galacci <bailey0522@gmail.com>
2020-03-13 16:06:28 -0600

    minor fixes/cleanups

--- models/dynamic_FIRFilter/sm_callback_stop.m
+++ models/dynamic_FIRFilter/sm_callback_stop.m
@@ -47,7 +47,7 @@ title('Left Data');
 
 % Spectrogram
 subplot(212);
-spectrogram(mp.left_data_out,8192,7800,8192,mp.Fs, 'yaxis');   % Display the spectrogram.
+%spectrogram(mp.left_data_out,8192,7800,8192,mp.Fs, 'yaxis');   % Display the spectrogram.
 ylim([1 2]);  
 title('HPF Filtered Speech Signal Chirp Spectrogram');
 print('lpfFilt_speech_chirp_signal', '-dpng');
