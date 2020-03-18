4e35b440850973e24eff64342e35668de8f1263a
Bailey Galacci <bailey0522@gmail.com>
2020-03-18 14:22:13 -0600

    first

--- models/dynamic_FIRFilter/sm_callback_init.m
+++ models/dynamic_FIRFilter/sm_callback_init.m
@@ -32,12 +32,12 @@ mp.Ts = 1/mp.Fs;  % sample period
 %% Set the FPGA system clock frequency (frequency of the FPGA fabric)
 % The system clock frequency should be an integer multiple of the Audio codec AD1939 Mclk frequency (12.288 MHz)
 if mp.fastsim_flag == 0
-    mp.Fs_system = 98304000;        % System clock frequency in Hz of Avalon Interface  Mclk*8 = 12.288MHz*8=98304000
+    mp.Fs_system = 98304000;            % System clock frequency in Hz of Avalon Interface  Mclk*8 = 12.288MHz*8=98304000
 else
-    mp.Fs_system = mp.Fs * mp.fastsim_Fs_system_N;          % Note: For faster development runs (faster sim times), reduce the number of system clocks between samples.  mp.fastsim_Fs_system_N is set in sm_run_me_first.m
+    mp.Fs_system = mp.Fs * mp.fastsim_Fs_system_N;      % Note: For faster development runs (faster sim times), reduce the number of system clocks between samples.  mp.fastsim_Fs_system_N is set in sm_run_me_first.m
 end
-mp.Ts_system   = 1/mp.Fs_system;         % System clock period
-mp.rate_change = mp.Fs_system/mp.Fs;   % how much faster the system clock is to the sample clock
+mp.Ts_system   = 1/mp.Fs_system;        % System clock period
+mp.rate_change = mp.Fs_system/mp.Fs;    % how much faster the system clock is to the sample clock
 
 %% Set the data type for audio signal (left and right channels) in data plane
 mp.W_bits = 32;  % Word length
@@ -47,9 +47,9 @@ mp.F_bits = 28;  % Number of fractional bits in word
 mp = sm_init_control_signals(mp);  % create the control signals
 
 %% Create test signals for the left and right channels
-mp = sm_init_test_signals(mp);  % create the test signals that will go through the model
-stop_time = mp.test_signal.duration;  % simulation time is based on the number of audio samples to go through the model
-if mp.sim_prompts == 1  % Note: sim_prompts is set in Run_me_first.m and is set to zero when hdl code generation is run
+mp = sm_init_test_signals(mp);          % create the test signals that will go through the model
+stop_time = mp.test_signal.duration;    % simulation time is based on the number of audio samples to go through the model
+if mp.sim_prompts == 1                  % Note: sim_prompts is set in Run_me_first.m and is set to zero when hdl code generation is run
     Nsamples_avalon = mp.test_signal.Nsamples * mp.rate_change;
     disp(['Simulation time has been set to ' num2str(stop_time) ' seconds'])
     disp(['    Processing ' num2str(Nsamples_avalon) ' Avalon streaming samples.'])
