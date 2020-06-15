9ca8f956192072ec90b106bd8009ffd91dacfe87
Trevor Vannoy <vannoy@flatearthinc.com>
2020-06-15 13:02:22 -0600

    update delay calcuation to match array index numbers
    the array is indexed like shown, when looking at the array:
    13 14 15 16
    9  10 11 12
    5  6  7  8
    1  2  3  4
    
    the x-axis is coming out of the page, and the coordinate system is
    right-handed

--- models/delay_and_sum_beamformer/computeDelays.m
+++ models/delay_and_sum_beamformer/computeDelays.m
@@ -14,11 +14,11 @@ azimuthRadians = mp.simulatedAzimuth * DEG_TO_RAD;
 elevationRadians = mp.simulatedElevation * DEG_TO_RAD;
 
 yDistanceFromPhaseCenter = ELEMENT_SPACING * (yElementIdx - (NUM_ELEMENTS_Y - 1)/2);
-zDistanceFromPhaseCenter = ELEMENT_SPACING * (zElementIdx - (NUM_ELEMENTS_Z - 1)/2) * -1;
+zDistanceFromPhaseCenter = ELEMENT_SPACING * (zElementIdx - (NUM_ELEMENTS_Z - 1)/2);
 
 idx = 0;
-for y = yElementIdx
-    for z = zElementIdx
+for z = zElementIdx
+    for y = yElementIdx
         idx = idx + 1;
         delays(idx) = -SAMPLING_RATE/SPEED_OF_SOUND  * (yDistanceFromPhaseCenter(y + 1) * sin(azimuthRadians) * cos(elevationRadians) + ...
             zDistanceFromPhaseCenter(z + 1) * sin(elevationRadians));
