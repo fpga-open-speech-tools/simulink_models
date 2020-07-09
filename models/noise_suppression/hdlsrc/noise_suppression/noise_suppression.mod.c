df0b71619c09dbc0ea04498bc3a6bef0dc20a595
Trevor Vannoy <trevorvannoy@montana.edu>
2020-07-08 20:17:30 -0600

    changes for code generation compatibility
    changed from floating point to fixed-point because delay balancing was proving difficult with the floating point blocks

---
+++ models/noise_suppression/hdlsrc/noise_suppression/noise_suppression.mod.c
@@ -0,0 +1,30 @@
+#include <linux/build-salt.h>
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(.gnu.linkonce.this_module) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+MODULE_INFO(depends, "");
+
+MODULE_ALIAS("of:N*T*Cdev,fe-noise_suppression");
+MODULE_ALIAS("of:N*T*Cdev,fe-noise_suppressionC*");
+
+MODULE_INFO(srcversion, "8D3949BC385190B0BA7DC2F");
