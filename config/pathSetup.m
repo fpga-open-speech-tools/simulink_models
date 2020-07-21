bcfd87be6125d7bf068095926756dc5b434926d8
Dylan Wickham <wickham@flatearthinc.com>
2020-07-21 13:03:41 -0600

    Refactored, added error messages, and (untested) Linux support

--- config/pathSetup.m
+++ config/pathSetup.m
@@ -18,30 +18,6 @@
 % Bozeman, MT 59718
 % support@flatearthinc.com
 
-%% Git Repository Setup
-% Add your git path to the cell array selection below so it will be found
-% the next time you run Simulink.
-% The first directory that Matlab finds that exists will be used.
-% localGitPath = {};
-% localGitPath{end + 1} = '/mnt/data/NIH';
-% localGitPath{end + 1} = 'C:\Users\bugsbunny\research\NIH';
-% localGitPath{end + 1} = 'V:\MSU\GitHub\';
-% localGitPath{end + 1} = 'C:\Users\wickh\Documents\NIH\Review';
-%localGitPath{end + 1} = '/home/trevor/research/NIH_SBIR_R44_DC015443';
-
-% validIndex = 0;
-% for index=1:length(localGitPath)
-%     if isfolder(localGitPath{index}) 
-%         validIndex = index;
-%         break
-%     end
-% end
-% if validIndex > 0
-%     gitPath = localGitPath{validIndex};
-% else
-%     error('Local Git repository not found.  Please add your Git path to pathSetupWindows.m');
-% end
-
 %% Setup Matlab/Simulink paths
 % These paths are required for developing simulink models and autogenerating code.
 % It is intended that users put the FPGA Open Speech Tools repositories
@@ -49,54 +25,20 @@
 
 % TODO: change struct field names to match coding style; this struct
 %       is used all over the place, so this will require some global refactoring
-config_dir = erase(mfilename('fullpath'), mfilename); 
-config_path = config_dir + "pathx.json";
-if isfile(config_path)
-    config = jsondecode(fileread(config_path));
-else
-    root_dir = config_dir + "../../";
-    [status, values] = fileattrib(root_dir);
-    root_dir = values.Name;
-    config.root = root_dir;
-    quartus_root = getenv("QUARTUS_ROOTDIR");
-    config.quartus = [quartus_root '/bin64/'];
-    disp(config.quartus)
-end
-disp("mfilename is: " + config_dir)
-gitPath = config.root;
+autogenConfig = getconfig();
+autogenRootDir = autogenConfig.root;
 mp.model_path = '';
 %mp.model_path           = [gitPath filesep 'simulink_models' filesep 'models' filesep mp.model_name];
-mp.test_signals_path    = [gitPath filesep 'simulink_models' filesep 'test_signals'];
-mp.ipcore_codegen_path  = [gitPath filesep 'simulink_codegen' filesep 'ipcore'];
-mp.driver_codegen_path  = [gitPath filesep 'simulink_codegen' filesep 'device_drivers'];
-mp.ui_codegen_path      = [gitPath filesep 'simulink_codegen' filesep 'ui'];
-mp.dtogen_path          = [gitPath filesep 'utils' filesep  'device_tree_overlays'];
-mp.codegen_path         = [gitPath filesep 'simulink_codegen' ];
+mp.test_signals_path    = [autogenRootDir filesep 'simulink_models' filesep 'test_signals'];
+mp.ipcore_codegen_path  = [autogenRootDir filesep 'simulink_codegen' filesep 'ipcore'];
+mp.driver_codegen_path  = [autogenRootDir filesep 'simulink_codegen' filesep 'device_drivers'];
+mp.ui_codegen_path      = [autogenRootDir filesep 'simulink_codegen' filesep 'ui'];
+mp.dtogen_path          = [autogenRootDir filesep 'utils' filesep  'device_tree_overlays'];
+mp.codegen_path         = [autogenRootDir filesep 'simulink_codegen' ];
 
 
 %% Quartus Setup
-% Add your Quartus path to the cell array selection below so it will be found
-% the next time you run Simulink.
-% The first directory that Matlab finds that exists will be used.
-% localQuartusPath = {};
-% localQuartusPath{end + 1} = 'C:\intelFPGA_lite\18.1\quartus\bin64';
-% localQuartusPath{end + 1} = 'D:\intelFPGA_lite\18.1\quartus\bin64';
-% localQuartusPath{end + 1} = 'C:\intelFPGA\18.0\quartus\bin64';
-% localQuartusPath{end + 1} = '/usr/local/intelFPGA/19.1/quartus/bin';
-% localQuartusPath{end + 1} = '/usr/local/intelFPGA_lite/18.0/quartus/bin';
-% 
-% validIndex = 0;
-% for index=1:length(localQuartusPath)
-%     if isfolder(localQuartusPath{index}) 
-%         validIndex = index;
-%     end
-% end
-% if validIndex > 0
-%     quartusPath = localQuartusPath{validIndex};
-% else
-%     error('Local Quartus install not found.  Please add your Quartus path to pathSetupWindows.m');
-% end
-mp.quartus_path = config.quartus;
+mp.quartus_path = autogenConfig.quartus;
 
 %% Setup Python paths
 if count(py.sys.path,mp.ipcore_codegen_path) == 0
@@ -121,7 +63,7 @@ hdlsetuptoolpath('ToolName', 'Altera Quartus II', 'ToolPath', mp.quartus_path);
 %% Print out the paths
 disp(['------------------------------------------------'])
 disp(['Setting up the the following path parameters'])
-disp(['Local GitHub repository path = ' gitPath])
+disp(['Local GitHub repository path = ' autogenRootDir])
 disp(['Simulink model path          = ' mp.model_path])
 disp(['Test signals path            = ' mp.test_signals_path])
 disp(['VHDL Codegen path            = ' mp.ipcore_codegen_path])
@@ -129,3 +71,40 @@ disp(['Driver Codegen path          = ' mp.driver_codegen_path])
 disp(['UI Codegen path              = ' mp.ui_codegen_path])
 disp(['Quartus path                 = ' mp.quartus_path])
 disp(['------------------------------------------------'])
+
+
+function config = getconfig()
+% Get the autogen configuration from path.json or automatically
+    config_dir = erase(mfilename('fullpath'), mfilename); 
+    config_path = config_dir + "path.json";
+    
+    if isfile(config_path)
+        config = jsondecode(fileread(config_path));
+        if isfolder(config.root) == 0
+            error("The given root path " + config.root + " was not found on your system.")
+        end
+        if isfolder(config.quartus) == 0
+           error("The given quartus path " + config.quartus + " was not found on your system.") 
+        end
+    else
+        root_dir = config_dir + ".." +  filesep + ".." + filesep;
+        [~, values] = fileattrib(root_dir);
+        root_dir = values.Name;
+        config.root = root_dir;
+        if ispc
+            quartus_root = strcat(getenv("QUARTUS_ROOTDIR"),filesep);
+            bin = "bin64";
+        else
+            % QUARTUS_ROOTDIR is not defined on Linux installations by default
+            qsys_root = getenv("QSYS_ROOTDIR");
+            temp = regexp(qsys_root, "sopc_builder", 'split');
+            quartus_root = temp{1};
+            bin = "bin";
+        end
+        if isempty(quartus_root)
+           error("Quartus was not found in your system environment variables.\n" ...
+               + "Ensure Quartus environment variables are set or use path.json to configure Quartus path.") 
+        end
+        config.quartus = char(quartus_root + bin + filesep);
+    end
+end
