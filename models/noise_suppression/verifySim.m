0dc84d828027ebf370df1bdaf64a354378ef2833
Trevor Vannoy <vannoy@flatearthinc.com>
2020-08-05 16:08:49 -0600

    use new init script framework

--- models/noise_suppression/verifySim.m
+++ models/noise_suppression/verifySim.m
@@ -27,8 +27,26 @@
 % Bozeman, MT 59718
 % openspeech@flatearthinc.com
 
-
 %% Verify that the test data got encoded, passed through the model, and
-plot(mp.Avalon_Source_Data);
-hold on;
-plot(data_out);
+% decoded correctly.  The input (modified by gain) and output values should be identical.
+
+close all;
+
+figure(1)
+subplot(2,1,1)
+plot(testSignal.audio(:,1)); hold on
+plot(dataOut.Data(:,1))
+title('Left channel')
+legend('input', 'output')
+
+subplot(2,1,2)
+plot(testSignal.audio(:,2)); hold on
+plot(dataOut.Data(:,2))
+title('Right channel')
+legend('input', 'output')
+
+% original_audio = [mp.test_signal.left(:) mp.test_signal.right(:)];
+% processed_audio = [mp.left_data_out(:) mp.right_data_out(:)];
+% soundsc(original_audio, mp.Fs);
+% pause(mp.test_signal.duration*1.1);
+% soundsc(processed_audio, mp.Fs);
