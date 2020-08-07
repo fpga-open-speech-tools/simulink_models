5ae9513af836ac8e3d03fdcface9e1773b3628ae
Trevor Vannoy <vannoy@flatearthinc.com>
2020-08-07 11:29:39 -0600

    Update FFT model for new init script framework
    There is still rampant use of the mp struct in the model due to
    copy/pasting parameters from the old init scripts; this should be
    cleaned up in the future.
    
    acoustic.wav was made into stereo to be compatible with AudioSource;
    this functionality could be added into AudioSource, or we could require
    that simulation audio files are stereo

--- models/FFT_Analysis_Synthesis/modelparameters.m
+++ models/FFT_Analysis_Synthesis/modelparameters.m
@@ -1,8 +1,30 @@
-function mp = sm_init_FFT_filters(mp)
 
-%----------------------------------------------------------------------------
-% Filter 1 - Low Pass
-%----------------------------------------------------------------------------
+%%% Autogen parameters
+
+mp.testFile = [mp.test_signals_path filesep 'acoustic.wav'];
+
+% TODO: use booleans instead of 0 and 1
+mp.sim_prompts = 1;
+mp.sim_verify = 1;
+mp.simDuration = 0.01;
+mp.nSamples = config.system.sampleClockFrequency * mp.simDuration;
+
+mp.useAvalonInterface = false;
+    
+%% Model parameters 
+mp.FFT_size = 128;
+% XXX: an unsigned int of mp.FFT_size_Nbits only goes up to mp.FFT_size - 1; not sure if that's intended or a bug 
+mp.FFT_size_Nbits = log2(mp.FFT_size);
+mp.FFT_size_half = mp.FFT_size/2;
+mp.FFT_frame_shift = mp.FFT_size/4;  % Changiing this from a divide by four implies substantial architectural changes
+mp.FFT_frame_shift_Nbits = log2(mp.FFT_frame_shift);
+
+mp.DPRAM1_size = mp.FFT_size*2;  % number of words
+mp.DPRAM1_address_size = log2(mp.DPRAM1_size);
+
+mp.SysRate_Upsample_Factor = mp.FFT_size/mp.FFT_frame_shift * 8 * 4;  % How much faster the fast clock must be to complete a FFT within the time of FFT_frame_shift number of samples
+
+%% Filter 1 - Low Pass
 mp.low_pass_cutoff = 4000;  % frequency in Hz
 mp.low_pass_index = ceil(mp.low_pass_cutoff / mp.Fs * mp.FFT_size) + 1;  % the +1 is to convert from the FFT bin array that is zero offset to a Matlab array that is 1 offset
 mp.fft_gains_1_real = zeros(mp.FFT_size_half,1);  % start by zeroing array
@@ -10,9 +32,7 @@ mp.fft_gains_1_real(1:mp.low_pass_index)  = ones(mp.low_pass_index,1);
 mp.fft_gains_1_imag  = zeros(mp.FFT_size_half,1);  % zero imaginary part
 mp.latency_threshold1 = sum(mp.fft_gains_1_real)/mp.FFT_size;
 
-%----------------------------------------------------------------------------
-% Filter 2 - Band Pass
-%----------------------------------------------------------------------------
+%% Filter 2 - Band Pass
 mp.band_pass_cutoff_low  = 4000;  % frequency in Hz
 mp.band_pass_cutoff_high = 8000;  % frequency in Hz
 mp.band_pass_index_low  = floor(mp.band_pass_cutoff_low / mp.Fs * mp.FFT_size) + 1;  % Note: The FFT bin indexing assumes the first bin index number of zero.  Matlab starts arrays with index 1.  Thus we need to add get the right frequency bin.
@@ -22,9 +42,7 @@ mp.fft_gains_2_real(mp.band_pass_index_low:mp.band_pass_index_high)  = ones(mp.b
 mp.fft_gains_2_imag  = zeros(mp.FFT_size_half,1);  % zero imaginary part
 mp.latency_threshold2 = sum(mp.fft_gains_2_real)/mp.FFT_size;
 
-%----------------------------------------------------------------------------
-% Filter 3 - High Pass
-%----------------------------------------------------------------------------
+%% Filter 3 - High Pass
 mp.high_pass_cutoff = 8000;  % frequency in Hz
 mp.high_pass_index = floor(mp.high_pass_cutoff / mp.Fs * mp.FFT_size) + 1;
 mp.fft_gains_3_real = zeros(mp.FFT_size_half,1);  % start by zeroing array
@@ -32,13 +50,7 @@ mp.fft_gains_3_real(mp.high_pass_index:end)  = ones(mp.FFT_size_half - mp.high_p
 mp.fft_gains_3_imag  = zeros(mp.FFT_size_half,1);  % zero imaginary part
 mp.latency_threshold3 = sum(mp.fft_gains_3_real)/mp.FFT_size;
 
-%----------------------------------------------------------------------------
-% Filter 4 - All Pass
-%----------------------------------------------------------------------------
+%% Filter 4 - All Pass
 mp.fft_gains_4_real = ones(mp.FFT_size_half,1);
 mp.fft_gains_4_imag  = zeros(mp.FFT_size_half,1);  % zero imaginary part
 mp.latency_threshold4 = sum(mp.fft_gains_4_real)/mp.FFT_size;
-
-
-end
-
