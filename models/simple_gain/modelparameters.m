0058be392fdc1559c344e082f814ff2f315a2d87
Dylan Wickham <wickham@flatearthinc.com>
2020-09-25 12:47:03 -0600

    Renamed Left_Gain to Gain, resized Gain, and removed unrelated code

--- models/simple_gain/modelparameters.m
+++ models/simple_gain/modelparameters.m
@@ -9,15 +9,3 @@ mp.sim_verify = 0;
 mp.simDuration = 5;
 mp.nSamples = config.system.sampleClockFrequency * mp.simDuration;
 
-%mp.useAvalonInterface = true;
-    
-%% Exponential moving average setup
-
-% 10 ms window
-mp.windowSize = 10e-3 * mp.Fs;
-
-% define the exponential moving average weight to be roughly equivalent to
-% a simple moving average of length mp.windowSize
-% https://en.wikipedia.org/wiki/Moving_average#Relationship_between_SMA_and_EMA
-mp.exponentialMovingAverageWeight = fi(2/(mp.windowSize + 1), 0, 32, 31);
-
