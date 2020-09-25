e9f5c15f9601d23dc4ba4a7648e007d165b96bf3
Dylan Wickham <61513109+fe-wickham@users.noreply.github.com>
2020-09-25 12:48:21 -0600

    Updated Simple Gain model to new framework with channel based processing (#45)
    * Updated Simple Gain model to new framework with channel based processing
    
    * Renamed Left_Gain to Gain, resized Gain, and removed unrelated code

--- models/simple_gain/verifySim.m
+++ models/simple_gain/verifySim.m
@@ -27,21 +27,36 @@
 % Bozeman, MT 59718
 % openspeech@flatearthinc.com
 
-function mp = sm_stop_verify(mp)
-
 %% Verify that the test data got encoded, passed through the model, and
 % decoded correctly.  The input (modified by gain) and output values should be identical.
 
-mp.left_error_max  = max(abs(mp.test_signal.left*mp.register(1).value-mp.left_data_out));
-mp.right_error_max = max(abs(mp.test_signal.left*mp.register(2).value-mp.right_data_out));
-mp.precision = 2^(-mp.F_bits);
+close all;
+
+figure(1)
+subplot(2,1,1)
+plot(testSignal.audio(:,1)); hold on
+plot(mp.dataOut(1, :))
+title('Left Gain')
+legend('input', 'output')
+
+subplot(2,1,2)
+plot(testSignal.audio(:,2)); hold on
+plot(mp.dataOut(2, :))
+title('Right Gain')
+legend('input', 'output')
+
+
+left_error_max  = max(abs(testSignal.audio(:,1)*mp.register{1}.value-mp.dataOut(1, :)'));
+right_error_max = max(abs(testSignal.audio(:,2)*mp.register{1}.value-mp.dataOut(2, :)'));
+precision = 2^(-mp.F_bits);
 % display popup message
-    str1 = [' Max Left Error = ' num2str(mp.left_error_max) '\n Max Right Error = ' num2str(mp.right_error_max)];
-    str1 = [str1 ' Max Left Error = ' num2str(mp.left_error_max) '\n Max Right Error = ' num2str(mp.right_error_max)];
-if (mp.left_error_max <= mp.precision) && (mp.right_error_max <= mp.precision)
+    str1 = [' Max Left Error = ' num2str(left_error_max) '\n Max Right Error = ' num2str(right_error_max)];
+    str1 = [str1 ' Max Left Error = ' num2str(left_error_max) '\n Max Right Error = ' num2str(right_error_max)];
+if (left_error_max <= precision) && (right_error_max <= precision)
     str1 = [str1 '\n Error is within exceptable range \n Least significant bit precision (F_bits = ' num2str(mp.F_bits) ') is ' num2str(2^(-mp.F_bits))];
     helpdlg(sprintf(str1),'Verification Message: Passed')
 else
     str1 = [str1 '\n Error is **NOT** within exceptable range \n Least significant bit precision (F_bits = ' num2str(mp.F_bits) ') is ' num2str(2^(-mp.F_bits))];
     helpdlg(sprintf(str1),'Verification Message: Failed')
 end
+
