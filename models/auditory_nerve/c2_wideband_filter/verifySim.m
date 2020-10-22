5c186771c4deea08324a21486c5d059f2c039640
Connor Dack <dack@flatearthinc.com>
2020-10-22 14:32:52 -0600

    Updated the Sim

--- models/auditory_nerve/c2_wideband_filter/verifySim.m
+++ models/auditory_nerve/c2_wideband_filter/verifySim.m
@@ -31,24 +31,24 @@
 % decoded correctly.  The input (modified by gain) and output values should be identical.
 
 close all;
+mex C2ChirpFilt.c complex.c
 
+c2_filter_out = zeros(length(data_input),1);
 data_input = testSignal.audio(:,1);
-% me_out = mef_verification(data_input, Fs, tdres);
+for i = 1:length(data_input)
+    c2_filter_out(i) = C2ChirpFilt(data_input(i),tdres,cf,i-1,taumaxc2,fcohcc2);
+end
 
 figure
 subplot(2,1,1)
 plot(testSignal.audio(:,1))
+legend('MEF Result Input Wave')
 title('Audio Input')
-legend('input')
 
-% sim_out = mp.dataOut(1, 1:end-1);
-% subplot(2,1,2)
-% plot(mp.dataOut(1, :))
-% hold on
-% plot(me_out)
-% legend('Simulink','meout')
-% title('Simulink Output vs Simulation')
-% 
-% [error,idx] = max(abs(sim_out - me_out));
-% sim_out(idx);
-% me_out(idx);
+sim_out = mp.dataOut(1, 1:end-1);
+subplot(2,1,2)
+plot(c2_filter_out)
+hold on
+plot(sim_out)
+legend('C Source Code','Simulink')
+title('C Source Code vs Simulink Output')
