3061e058f055869a9c6fc68f4004106cfa243a90
Connor Dack <dack@flatearthinc.com>
2020-10-28 12:19:00 -0600

    Initial Commit

--- models/auditory_nerve/ohc_calc_tau/modelparameters.m
+++ models/auditory_nerve/ohc_calc_tau/modelparameters.m
@@ -13,28 +13,4 @@ Fs = 48e3;    % Sampling frequency
 tdres = 1/Fs; % Binsize in seconds
 nrep = 100;   % Number of repititions for peri-stimulus time histogram
 
-%% OHC Nonlinear Filter
-ohcasym = 7.0; % Ratio of positive Max to negative Max
-minR = 0.05; % Declared as constant in function
-% Max and min time constants (input as bmTaumax and bmTaumin in source code, chosen as outputs for cf = 1000 Hz from Get_taubm)
-taumax = 0.003;
-taumin = 4.6310e-04;
-
-% *** Adding precalculated values to avoid unnecessary computation and
-% switch block architecture (All the following lines until Test Signal
-% section)
-% *** Added by Matthew Blunt 04/08/2020
-
-R = taumin/taumax;
-
-if R < minR
-    minR = 0.5*R;
-else
-    minR = minR;
-end
-
-dc = (ohcasym-1)/(ohcasym+1.0)/2.0-minR;
-R1 = R-minR;
-
-% Denoted in C code: For new nonlinearity
-s0 = -dc/log(R1/(1-minR));
+%% OHC Calculate Tau for C1
