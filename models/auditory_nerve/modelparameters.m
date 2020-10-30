3b2a7cf55b2ceeada6edcf2cfa5c5b76064de20c
Connor Dack <dack@flatearthinc.com>
2020-10-30 15:27:03 -0600

    Created C1 Chirp Parameters Function

--- models/auditory_nerve/modelparameters.m
+++ models/auditory_nerve/modelparameters.m
@@ -23,68 +23,14 @@ cihc = 1;
 mef_parameters;
 
 %% C1 Chirp Filter Parameters
-rsigma = .5;     % Pole shifting constant (set as constant for testing)
+rsigma = .5;    % Pole shifting constant (set as constant for testing)
 taumax = 0.003; % Max time constant (given as bmTaumax in C source code, set to 0.003 for testing, the value of bmTaumax for cf = 1000)
-
-%--Calculate the constant parameters for the C1 filter, including initial pole/zero locations, shifting constants, and gain constants
-% Setup locations of poles and zeros
-sigma0 = 1/taumax;
-ipw    = 1.01*cf*2*pi-50;
-ipb    = 0.2343*2*pi*cf-1104;
-rpa    = 10^(log10(cf)*0.9 + 0.55)+ 2000;
-pzero  = 10^(log10(cf)*0.7+1.6)+500;
-    
-order_of_pole    = 10;             
-half_order_pole  = order_of_pole/2;
-order_of_zero    = half_order_pole;
-    
-fs_bilinear = 2*pi*cf/tan(2*pi*cf*tdres/2);
-rzero       = -pzero;
-CF          = 2*pi*cf;
-
-preal = zeros(11,1);
-pimag = zeros(11,1);
-
-preal(1) = -sigma0;
-preal(5) = preal(1) - rpa;
-preal(3) = (preal(1) + preal(5))/2;
-preal(7) = preal(1);
-preal(9) = preal(5);
-preal(2) = preal(1);
-preal(4) = preal(3);
-preal(6) = preal(5);
-preal(8) = preal(2);
-preal(10) = preal(6);
-
-pimag(1) = ipw;
-pimag(5) = pimag(1) - ipb;
-pimag(3) = (pimag(1) + pimag(5))/2;
-pimag(7) = pimag(1);
-pimag(9) = pimag(5);
-pimag(2) = -pimag(1);
-pimag(4) = -pimag(3);
-pimag(6) = -pimag(5);
-pimag(8) = pimag(2);
-pimag(10) = pimag(6);
-
-% Calculate initial phase
-C1initphase = 0.0;
-for i = 1:half_order_pole
-    C1initphase = C1initphase + atan(CF/(-rzero))-atan((CF-pimag(2*i-1))/(-preal(2*i-1)))-atan((CF+pimag(2*i-1))/(-preal(2*i-1)));
-end
-
-% Normalize the gain
-C1gain_norm = 1.0;
-for i = 1:order_of_pole
-        C1gain_norm = C1gain_norm*((CF-pimag(i))^2 + (preal(i))^2);
-end
-
-norm_gainc1 = (sqrt(C1gain_norm))/((sqrt(CF^2+rzero^2))^order_of_zero);
+[order_of_zero, fs_bilinear, CF, preal, pimag, C1initphase, norm_gainc1] = c1_chirp_parameter(cf, tdres, taumax);
 
 %% C2 Wideband Filter
 taumaxc2 = 0.0030; % time constant determined with another function (chosen as the output of Get_taubm for cf = 1000 Hz)     
 fcohcc2 = 1;       % parameter calculated as 1/ratiobm in model_IHC_BEZ2018.c (arbitrary for initial test)
-[C2coeffs, norm_gainc2] = C2Coefficients( tdres, cf, taumaxc2, fcohcc2); % Calculating IIR Biquad Coefficients by calling C2Coefficients MATLAB function
+[C2coeffs, norm_gainc2] = C2Coefficients(tdres, cf, taumaxc2, fcohcc2); % Calculating IIR Biquad Coefficients by calling C2Coefficients MATLAB function
 
 %% Inner Hair Cell Parameters
 % IHC Nonlinear Log Function Parameters
