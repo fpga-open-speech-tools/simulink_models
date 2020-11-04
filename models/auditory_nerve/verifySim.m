e32ce3b3cb1d5551eb2d0188215dcef80a240fe3
Connor Dack <dack@flatearthinc.com>
2020-11-04 09:40:08 -0700

    Simulation of subsystem
    Added 'To Workspace' Blocks to allow for plotting of intermediate steps

--- models/auditory_nerve/verifySim.m
+++ models/auditory_nerve/verifySim.m
@@ -31,18 +31,18 @@
 close all;
 
 data_input            = testSignal.audio(:,1);
+totalstim             = length(data_input);
+
+wbout                 = zeros(1,totalstim);
+ohc_sim               = zeros(1,totalstim);
+c_grdelay             = zeros(1,totalstim);
+c_rsigma              = zeros(1,totalstim);
+c_tauc1               = zeros(1,totalstim);
+c_tauwb               = zeros(1,totalstim);
+c_wbgain              = zeros(1,totalstim);
 c1_chirp_filter       = zeros(1,totalstim);
 c2_wideband_filter    = zeros(1,totalstim);
 inner_hair_cell_out   = zeros(1,totalstim);
-
-totalstim  = length(data_input);
-wbout      = zeros(1,totalstim);
-ohc_sim    = zeros(1,totalstim);
-c_grdelay   = zeros(1,totalstim);
-c_rsigma   = zeros(1,totalstim);
-c_tauc1   = zeros(1,totalstim);
-c_tauwb   = zeros(1,totalstim);
-c_wbgain   = zeros(1,totalstim);
 
 %% Complete Auditory Nerve
 mex C1ChirpFilt.c complex.c
@@ -61,8 +61,8 @@ for i = 1:totalstim
     end
     ohc_sim(1,i) = outer_hair_cell_source(wbout(1,i), ohcasym, s0, s1, x1, tdres, Fcohc, i-1, gainohc, orderohc, taumin, taumax);
     [c_grdelay(1,i), c_rsigma(1,i), c_tauc1(1,i), c_tauwb(1,i), c_wbgain(1,i)] = calc_tau_source(tdres, cf, centerfreq, ohc_sim(1,i));
-    c1_chirp_filter(1,i) = C1ChirpFilt(middle_ear_filter_out(i), tdres, cf, i-1, taumax, c_rsigma(1,i));
-    c2_wideband_filter(1,i) = C2ChirpFilt(middle_ear_filter_out(i), tdres, cf, i-1, taumaxc2, fcohcc2);
+    c1_chirp_filter(1,i)     = C1ChirpFilt(middle_ear_filter_out(i), tdres, cf, i-1, taumax, c_rsigma(1,i));
+    c2_wideband_filter(1,i)  = C2ChirpFilt(middle_ear_filter_out(i), tdres, cf, i-1, taumaxc2, fcohcc2);
     inner_hair_cell_out(1,i) = inner_hair_cell_source(c1_chirp_filter(i), slope_c1, ihcasym_c1, c2_wideband_filter(i), slope_c2, ihcasym_c2, tdres, Fcihc, i-1, gainihc, orderihc);
 end
 pla_nl_out = NLBeforePLA(inner_hair_cell_out, totalstim, spont, cf);
@@ -70,13 +70,53 @@ syn_out    = PowerLaw(pla_nl_out, totalstim, randNums, Fs);
 
 %% Plot the Results
 figure
-subplot(2,1,1)
-plot(middle_ear_filter_out)
+subplot(7,1,1)
+plot(data_input)
 legend('Input Wave')
 title('Audio Input')
 
+mef_sim = double(squeeze(mef_sim_out.Data));
+subplot(7,1,2)
+plot(middle_ear_filter_out)
+hold on
+plot(mef_sim,'--')
+legend('C Source Code','Simulink')
+title('Middle Ear Filter Simulation')
+
+rsigma_sim = double(squeeze(rsigma_sim_out.Data));
+subplot(7,1,3)
+plot(c_rsigma)
+hold on
+plot(rsigma_sim,'--')
+legend('C Source Code','Simulink')
+title('R Sigma Simulation')
+
+c1_chirp_sim = double(squeeze(c1_chirp_sim_out.Data));
+subplot(7,1,4)
+plot(c1_chirp_filter)
+hold on
+plot(c1_chirp_sim,'--')
+legend('C Source Code','Simulink')
+title('C1 Chirp Filter Simulation')
+
+c2_wbgt_sim = double(squeeze(c2_wb_sim_out.Data));
+subplot(7,1,5)
+plot(c2_wideband_filter)
+hold on
+plot(c2_wbgt_sim,'--')
+legend('C Source Code','Simulink')
+title('C2 Wideband Gammatone Filter Simulation')
+
+ihc_sim = double(squeeze(ihc_sim_out.Data));
+subplot(7,1,6)
+plot(inner_hair_cell_out)
+hold on
+plot(ihc_sim,'--')
+legend('C Source Code','Simulink')
+title('Inner Hair Cell Simulation')
+
 sim_out = mp.dataOut;
-subplot(2,1,2)
+subplot(7,1,7)
 plot(syn_out)
 hold on
 plot(sim_out,'--')
