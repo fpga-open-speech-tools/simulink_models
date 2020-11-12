ae1f49e5126bada552817ec796fec3543363dccf
Tyler Davis <davis@flatearthinc.com>
2020-11-12 13:59:41 -0700

    Updated model logic, added input array creation to modelparameters

--- models/dpr_test/modelparameters.m
+++ models/dpr_test/modelparameters.m
@@ -2,9 +2,21 @@
 %% Autogen parameters
 
 mp.testFile = [mp.test_signals_path filesep 'noisySpeech.wav'];
-
-% TODO: use booleans instead of 0 and 1
 mp.sim_prompts = 1;
 mp.sim_verify = 1;
 mp.simDuration = 15;
 mp.nSamples = config.system.sampleClockFrequency * mp.simDuration;
+
+nAddresses = 512;
+n_shift = 16;
+
+input_data = 1:nAddresses;
+
+% Set the addresses for the DPR
+dprDataIn = (1:nAddresses)-1;
+
+% Shift the addresses into the correct location
+dprDataIn = bitshift(dprDataIn, n_shift);
+
+% Add the coefficients to the variables
+dprDataIn = dprDataIn + input_data;
