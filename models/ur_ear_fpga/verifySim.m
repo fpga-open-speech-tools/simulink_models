86461cc8a67b0b4c912f8303df383aa3be816c72
Connor Dack <dack@flatearthinc.com>
2020-11-13 12:57:05 -0700

    Added Debug Mode

--- models/ur_ear_fpga/verifySim.m
+++ models/ur_ear_fpga/verifySim.m
@@ -34,49 +34,70 @@ data_input = testSignal.audio(:,1);
 total_stim = length(data_input);
 rep_time   = total_stim * tdres;
 
-%% UR EAR Source 
-mex 'validation\model_IHC_BEZ2018.c' 'validation\complex.c'                             % Compile the UR EAR IHC BEZ2018 Model
-[mef_source_out, rsigma_source_out, c1_chirp_source_out, c2_wbgt_source_out, anm_source_out] = model_IHC_BEZ2018(data_input', cf, 1, tdres, rep_time, cohc, cihc, 2); % Simulate the Auditory Nerve Model
+debug_flag = 0;
 
+if(debug_flag == 0)
+    % Debug Flag == 0 
+    mex 'validation\model_IHC_BEZ2018.c' 'validation\complex.c'                             % Compile the UR EAR IHC BEZ2018 Model
+    anm_source_out = model_IHC_BEZ2018(data_input', cf, 1, tdres, rep_time, cohc, cihc, 2); % Simulate the Auditory Nerve Model
 
-%% Plot the Results
-figure
-subplot(6,1,1)
-plot(data_input)
-title('Audio Input')
+    % Plot the Results
+    figure
+    subplot(2,1,1)
+    plot(data_input)
+    title('Audio Input')
 
-subplot(6,1,2)
-plot(mef_source_out)
-hold on
-plot(mef_out_sim,'--')
-legend('C Source Code','Simulink')
-title('UR EAR - Middle Ear Filter Simulation')
+    subplot(2,1,2)
+    plot(anm_source_out(1,93:end))
+    hold on
+    plot(an_sim_out,'--')
+    legend('C Source Code','Simulink')
+    title('UR EAR - Auditory Nerve Simulation')
+    
+elseif(debug_flag == 1)   
+    % Debug Flag == 1 
+    mex 'validation\model_IHC_BEZ2018_debug.c' 'validation\complex.c'                             % Compile the UR EAR IHC BEZ2018 Model
+    [mef_source_out, rsigma_source_out, c1_chirp_source_out, c2_wbgt_source_out, anm_source_out] = model_IHC_BEZ2018_debug(data_input', cf, 1, tdres, rep_time, cohc, cihc, 2); % Simulate the Auditory Nerve Model
 
-subplot(6,1,3)
-plot(rsigma_source_out)
-hold on
-plot(rsigma_sim_out,'--')
-legend('C Source Code','Simulink')
-title('UR EAR - R Sigma Simulation')
+    % Plot the Results
+    figure
+    subplot(6,1,1)
+    plot(data_input)
+    title('Audio Input')
 
-subplot(6,1,4)
-plot(c1_chirp_source_out)
-hold on
-plot(c1_chirp_sim_out,'--')
-legend('C Source Code','Simulink')
-title('UR EAR - C1 Filter Simulation')
+    subplot(6,1,2)
+    plot(mef_source_out)
+    hold on
+    plot(mef_out_sim,'--')
+    legend('C Source Code','Simulink')
+    title('UR EAR - Middle Ear Filter Simulation')
 
-subplot(6,1,5)
-plot(c2_wbgt_source_out)
-hold on
-plot(c2_wbgt_sim_out,'--')
-legend('C Source Code','Simulink')
-title('UR EAR - C2 Filter Simulation')
+    subplot(6,1,3)
+    plot(rsigma_source_out)
+    hold on
+    plot(rsigma_sim_out,'--')
+    legend('C Source Code','Simulink')
+    title('UR EAR - R Sigma Simulation')
 
-subplot(6,1,6)
-plot(anm_source_out(1,93:end))
-hold on
-plot(an_sim_out,'--')
-legend('C Source Code','Simulink')
-title('UR EAR - Auditory Nerve Simulation')
+    subplot(6,1,4)
+    plot(c1_chirp_source_out)
+    hold on
+    plot(c1_chirp_sim_out,'--')
+    legend('C Source Code','Simulink')
+    title('UR EAR - C1 Filter Simulation')
+
+    subplot(6,1,5)
+    plot(c2_wbgt_source_out)
+    hold on
+    plot(c2_wbgt_sim_out,'--')
+    legend('C Source Code','Simulink')
+    title('UR EAR - C2 Filter Simulation')
+
+    subplot(6,1,6)
+    plot(anm_source_out(1,93:end))
+    hold on
+    plot(an_sim_out,'--')
+    legend('C Source Code','Simulink')
+    title('UR EAR - Auditory Nerve Simulation')
+end
 
