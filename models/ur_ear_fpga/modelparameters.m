ba3620e1fba61ec3658b847287fbe9b6d9f26831
Connor Dack <dack@flatearthinc.com>
2020-11-18 15:28:09 -0700

    Added Syanpse-Spike Gen
    Updated the UR EAR FPGA Model with Synapse-Spike Gen
    Updated the Model Parameters and Verify Sim Scripts

--- models/ur_ear_fpga/modelparameters.m
+++ models/ur_ear_fpga/modelparameters.m
@@ -18,7 +18,6 @@ order   = 3;    % Line 248 of model_IHC_BEZ2018.c
 % Impairment constants
 cohc  = 1;    % outer hair cell impairment constant ( from 0 to 1 )
 cihc  = 1;    % inner hair cell impairment constant ( from 0 to 1 )
-
 %% Middle Ear Filter Parameters
 [MEcoeffs, MEscale] = middle_ear_filter_parameter(tdres);
 
@@ -80,3 +79,129 @@ else
 end
 delay_point = max(0,ceil(delay/tdres)); % Line 374
     
+%% Synapse and Spike Gen Parameters
+% Synapse Parameters
+dt = tdres; 
+noiseType = 1;
+implnt = 0;
+spont = 100; % Spontaneous firing rate of neurons, as set in testANmodel_BEZ2018.m source code
+
+% Nonlinear PLA Filter Parameters
+cfslope = (spont^0.19)*(10^-0.87);
+cfconst = 0.1*(log10(spont))^2 + 0.56*log10(spont) - 0.84;
+cfsat = 10^(cfslope*8965.5/1e3 + cfconst);
+cf_factor = min(cfsat, 10^(cfslope*cf/1e3 + cfconst)) * 2.0;
+
+multFac = max(2.95*max(1.0,1.5-spont/100), 4.3-0.2*cf/1e3);
+
+% ---------------------------------------%
+% NOTE: delaypoint may need to be changed due
+%       to sampling rate change from 100 kHz 
+%       to 48 kHz, not sure yet
+% ---------------------------------------%
+delaypoint = floor(7500/(cf/1e3));
+
+% Power Law Adapter Parameters
+% Random Noise
+Hinput = 0.9;  % Set Hurst index to the value hard-coded in C source code
+spont = 100;   % Set mean of noise
+% Call fast fractional gaussian noise function for Double Precision
+randNums = single(ffGn(mp.nSamples, tdres, Hinput, noiseType, spont));
+
+% Parameters for Fast Power Law Adaptation Function
+alpha1 = 1.5e-6*100e3; 
+beta1 = 5e-4; 
+    
+% Parameters for Slow Power Law Adaptation Function
+alpha2 = 1e-2*100e3; 
+beta2 = 1e-1; 
+
+% Initialized outputs of PLA
+I1 = 0;
+I2 = 0;
+
+% Fast PLA 6th Order IIR Filter Approximation Coefficients
+% First 2nd Order Stage
+fast_b0_1 = 1;
+fast_b1_1 = -0.994466986569624;
+fast_b2_1 = 0.000000000002347;
+fast_a0_1 = 1;
+fast_a1_1 = -1.992127932802320;
+fast_a2_1 = 0.992140616993846;
+% Second 2nd Order Stage
+fast_b0_2 = 1;
+fast_b1_2 = -1.997855276593802;
+fast_b2_2 = 0.997855827934345;
+fast_a0_2 = 1;
+fast_a1_2 = -1.999195329360981;
+fast_a2_2 = 0.999195402928777;
+% Third 2nd Order Stage
+fast_b0_3 = 1;
+fast_b1_3 = 0.798261718184977;
+fast_b2_3 = 0.199131619874064;
+fast_a0_3 = 1;
+fast_a1_3 = 0.798261718183851;
+fast_a2_3 = 0.199131619873480;
+% Combine Coefficients into Matrix for Implementation in Simulink
+fast_coeffs = [fast_b0_1*10^-3 fast_b1_1*10^-3 fast_b2_1*10^-3 fast_a0_1 fast_a1_1 fast_a2_1; fast_b0_2 fast_b1_2 fast_b2_2 fast_a0_2 fast_a1_2 fast_a2_2; fast_b0_3 fast_b1_3 fast_b2_3 fast_a0_3 fast_a1_3 fast_a2_3];
+
+% Slow PLA 10th Order IIR Filter Approximation Coefficients
+% First 2nd Order Stage
+slow_b0_1 = 1;
+slow_b1_1 = -0.173492003319319;
+slow_b2_1 = 0.000000172983796;
+slow_a0_1 = 1;
+slow_a1_1 = -0.491115852967412;
+slow_a2_1 =  0.055050209956838;
+% Second 2nd Order Stage
+slow_b0_2 = 1;
+slow_b1_2 = -0.803462163297112;
+slow_b2_2 = 0.154962026341513;
+slow_a0_2 = 1;
+slow_a1_2 = -1.084520302502860;
+slow_a2_2 = 0.288760329320566;
+% Third 2nd Order Stage
+slow_b0_3 = 1;
+slow_b1_3 = - 1.416084732997016;
+slow_b2_3 = 0.496615555008723;
+slow_a0_3 = 1;
+slow_a1_3 = -1.588427084535629;
+slow_a2_3 = 0.628138993662508;
+% Fourth 2nd Order Stage
+slow_b0_4= 1;
+slow_b1_4 = -1.830362725074550;
+slow_b2_4 = 0.836399964176882;
+slow_a0_4 = 1;
+slow_a1_4 = -1.886287488516458;
+slow_a2_4 = 0.888972875389923;
+% Fifth 2nd Order Stage
+slow_b0_5 = 1;
+slow_b1_5 = -1.983165053215032;
+slow_b2_5 = 0.983193027347456;
+slow_a0_5 = 1;
+slow_a1_5 = -1.989549282714008;
+slow_a2_5 = 0.989558985673023;
+% Combine Coefficients into Matrix for Implementation in Simulink
+slow_coeffs = [slow_b0_1*0.2 slow_b1_1*0.2 slow_b2_1*0.2 slow_a0_1 slow_a1_1 slow_a2_1; slow_b0_2 slow_b1_2 slow_b2_2 slow_a0_2 slow_a1_2 slow_a2_2; slow_b0_3 slow_b1_3 slow_b2_3 slow_a0_3 slow_a1_3 slow_a2_3; slow_b0_4 slow_b1_4 slow_b2_4 slow_a0_4 slow_a1_4 slow_a2_4; slow_b0_5 slow_b1_5 slow_b2_5 slow_a0_5 slow_a1_5 slow_a2_5];
+
+
+% Spike Gen Parameters
+spont = 100;
+nSites = 4;
+t_rd_rest = 14.0e-3;
+t_rd_jump = 0.4e-3;
+t_rd_init = t_rd_rest + 0.02e-3*spont - t_rd_jump;
+tau = 60e-3;    
+
+trel = 10e-3;
+tabs = 10e-3;
+
+elapsed_time = 0; % single(tdres * randi(floor(Fs * t_rd_init)));
+% unitRateInterval = single(-log(rand(1))/tdres);
+unitRateInterval = 87745;
+% oneSiteRedock = single(-t_rd_init * log(rand(1)));
+oneSiteRedock = single(t_rd_init/2);
+
+% Accumulator Settings
+integrationTime = 40e-3*Fs;
+threshold       = 0.950;
--- models/ur_ear_fpga/verifySim.m
+++ models/ur_ear_fpga/verifySim.m
@@ -22,19 +22,19 @@
 % ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 %
 % Connor Dack
-% Audio Logic
+% AudioLogic, Inc
 % 985 Technology Blvd
 % Bozeman, MT 59718
 % openspeech@flatearthinc.com
 
 %% 
-% close all;
+close all;
 
 data_input = testSignal.audio(:,1);
 total_stim = length(data_input);
 rep_time   = total_stim * tdres;
 
-debug_flag = 0;
+debug_flag = 1;
 
 if(debug_flag == 0)
     % Disable the Debug Outputs
@@ -56,15 +56,26 @@ if(debug_flag == 0)
     
 elseif(debug_flag == 1)   
     % Enable the Intermediate Steps and Plotting
+    total_mean_rate = sum(data_input/length(data_input));
+    MaxArraySizeSpikes = length(data_input)*nrep;
     mex 'validation\model_IHC_BEZ2018_debug.c' 'validation\complex.c' % Compile the UR EAR IHC BEZ2018 Debug Source
+    mex 'synapse_spike_generator_model\spike_generator\spikegen_pseudorandom.c' 'synapse_spike_generator_model\complex.c'
+    
     [mef_source_out, rsigma_source_out, c1_chirp_source_out, c2_wbgt_source_out, c1_nl_source_out, c2_nl_source_out, ihc_source_out, anm_source_out] = model_IHC_BEZ2018_debug(data_input', cf, 1, tdres, rep_time, cohc, cihc, 2); % Simulate the Auditory Nerve Model
-
-    % Plot the Results
+    pla_nl_out = NLBeforePLA(anm_source_out, total_stim, spont, cf);
+    syn_out    = PowerLaw(pla_nl_out, total_stim, randNums, Fs);
+    [spCount_source, sptime_source, trd_vector_source, sp_count_redock_1, sp_count_redock_2, sp_count_redock_3, sp_count_redock_4] = spikegen_pseudorandom(...
+        syn_out, randNums, tdres, t_rd_rest, t_rd_init, tau, t_rd_jump, nSites, tabs, trel, spont, total_stim, nrep, total_mean_rate, MaxArraySizeSpikes, double(unitRateInterval), double(oneSiteRedock));
+    [counts, valid] = integrateCounts(integrationTime,sp_count_redock_1,sp_count_redock_2,sp_count_redock_3,sp_count_redock_4);
+    
+    %---------------------------------
+    %---Plot the Results--------------
     figure
     subplot(2,1,1)
     plot(data_input)
     title('Audio Input')
 
+    %--Middle Ear Filter Validation---
     subplot(2,1,2)
     plot(mef_source_out)
     hold on
@@ -72,6 +83,7 @@ elseif(debug_flag == 1)
     legend('C Source Code','Simulink')
     title('UR EAR - Middle Ear Filter Simulation')
     
+    %--Auditory Nerve Validation---
     figure
     subplot(7,1,1)
     plot(rsigma_source_out)
@@ -121,5 +133,65 @@ elseif(debug_flag == 1)
     plot(an_sim_out,'--')
     legend('C Source Code','Simulink')
     title(['UR EAR - Auditory Nerve Simulation: Char Freq = ' num2str(cf) ' Hz'])
+    
+    %--Synapse and Spike Gen Validation---
+    figure
+    subplot(8,1,1)
+    plot(pla_nl_out)
+    hold on
+    plot(nl_pla_sim_out, '--')
+    legend('MATLAB Function','Simulink')
+    title('Non-linear PLA Filter Simulation')
+
+    subplot(8,1,2)
+    plot(syn_out)
+    hold on
+    plot(syn_sim_out,'--')
+    legend('MATLAB Function','Simulink')
+    title('PLA Simulation')
+
+    %--Spike Gen Results---
+    subplot(8,1,3)
+    plot(sp_count_redock_1)
+    hold on
+    plot(sp_count_redock_1_sim,'--')
+    legend('C Source','Simulink')
+    title('Spike Count: Redock Site 1')
+
+    subplot(8,1,4)
+    plot(sp_count_redock_2)
+    hold on
+    plot(sp_count_redock_2_sim,'--')
+    legend('C Source','Simulink')
+    title('Spike Count: Redock Site 2')
+
+    subplot(8,1,5)
+    plot(sp_count_redock_3)
+    hold on
+    plot(sp_count_redock_3_sim,'--')
+    legend('C Source','Simulink')
+    title('Spike Count: Redock Site 3')
+
+    subplot(8,1,6)
+    plot(sp_count_redock_4)
+    hold on
+    plot(sp_count_redock_4_sim,'--')
+    legend('C Source','Simulink')
+    title('Spike Count: Redock Site 4')
+
+    %--Accumulator Results---
+    subplot(8,1,7)
+    plot(counts)
+    hold on
+    plot(spike_count_sim_out,'--')
+    legend('MATLAB Function','Simulink')
+    title('Accumulator: Counts')
+
+    subplot(8,1,8)
+    plot(valid)
+    hold on
+    plot(spike_valid_sim_out,'--')
+    legend('MATLAB Function','Simulink')
+    title('Accumulator: Valid')
 end
 
