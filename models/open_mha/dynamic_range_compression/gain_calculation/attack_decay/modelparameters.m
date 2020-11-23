42029986355f934c1dad7235e454111e2942fdfa
Connor Dack <dack@flatearthinc.com>
2020-11-23 16:58:41 -0700

    Working on validating the Attack-Decay Block
    Updated the Simulation File
    Updated the model paramters from the subsystems
    Updated the verify with the results and plotting from the subsystems

--- models/open_mha/dynamic_range_compression/gain_calculation/attack_decay/modelparameters.m
+++ models/open_mha/dynamic_range_compression/gain_calculation/attack_decay/modelparameters.m
@@ -15,32 +15,36 @@
 % openspeech@flatearthinc.com
 
 %% Initialize
-addpath(genpath('..\attack_decay_filter'));        % Location of the o1_lp_coeffs function
-addpath(genpath('..\..\..\referenced_functions')); % Location of the Dual Port RAM
+addpath(genpath('..\attack_decay_filter'));           % Attack and Decay Filter
+addpath(genpath('..\calculate_filter_coefficients')); % Attack and Decay Filter Coefficients
+addpath(genpath('..\..\..\referenced_functions'));    % Dual Port RAM - Frost Library
 
 %% Autogen parameters
 mp.testFile = [mp.test_signals_path filesep 'auditory_nerve\mef_result_subset.wav'];
 
-% Open MHA Parameters 
-fs = 48e3;
-coeff_size = 8;
-num_bands  = 8;
-num_coeff  = 2;
-
-% Data Input Array
-band_num_input = [1:1:num_bands num_bands:-1:1];
-band_num_input = [band_num_input band_num_input band_num_input band_num_input band_num_input];
-
 mp.sim_prompts = 1;
-mp.sim_verify = 1;
+mp.sim_verify  = 1;
 mp.simDuration = 15;
-mp.nSamples = length(band_num_input);
+mp.nSamples    = config.system.sampleClockFrequency * mp.simDuration;
 mp.useAvalonInterface = false;
 
-mp.W_bits = 24;
-mp.F_bits = 23;
+%% Open MHA Parameters 
+fs         = 48e3; % Samplig Frequency
+coeff_size = 8;    % Coefficient Address Size
+num_bands  = 8;    % Number of Frequency Bands
+num_coeff  = 2;    % Number of C1 Coefficients required by the Attack and Decay Filter
 
-%% Attack and Decay DPRAM Parameters
+%% Band Number Array
+stim_length         = AudioSource.fromFile(mp.testFile, mp.Fs, mp.nSamples).nSamples;
+band_num_input      = zeros(stim_length,1);
+for i = 1:stim_length
+    band_num_input(i,1) = mod(i-1,num_bands) + 1;
+end
+
+time = (0:1:length(band_num_input)-1) * 1/fs;
+band_num_timeseries = timeseries(band_num_input,time);
+
+%% Attack and Decay DP-RAM Parameters
 % Create tau values to generate C1 Coefficients between 0 and 1
 tau_as = -1 ./ (log([0:1:num_bands-1] ./ num_bands) * fs);
 tau_ds = -1 ./ (log([num_bands-1:-1:0] ./ num_bands) * fs);
@@ -56,6 +60,8 @@ for i = 1:2:2*num_bands-1
     ad_coeffs(i+1,1) = fi(decay_c1,0,16,16);
     z = z+1;
 end
-time = (0:1:length(band_num_input)-1) *1/fs;
-band_num_timeseries = timeseries(uint8(band_num_input),time);
+
+%% Attack and Decay Delay Block Paramters
+buf_a = ones(num_bands,1) * 65; % Initial Condition of the Attack Filter Delay Block
+buf_d = ones(num_bands,1) * 65; % Initial Condition of the Delay Filter Delay Block
 
--- models/open_mha/dynamic_range_compression/gain_calculation/attack_decay/verifySim.m
+++ models/open_mha/dynamic_range_compression/gain_calculation/attack_decay/verifySim.m
@@ -29,15 +29,24 @@
 
 %% Initialization
 close all;
-total_stim         = length(band_num_input);
-addr_attack_matlab = zeros(total_stim,1);
-addr_decay_matlab  = zeros(total_stim,1);
-c1_a_matlab        = zeros(total_stim,1);
-c2_a_matlab        = zeros(total_stim,1);
-c1_d_matlab        = zeros(total_stim,1);
-c2_d_matlab        = zeros(total_stim,1);
+data_input = testSignal.audio(:,1);
+total_stim = length(data_input);
+% Coefficient Arrays
+addr_attack_matlab   = zeros(total_stim,1);
+addr_decay_matlab    = zeros(total_stim,1);
+c1_a_matlab          = zeros(total_stim,1);
+c2_a_matlab          = zeros(total_stim,1);
+c1_d_matlab          = zeros(total_stim,1);
+c2_d_matlab          = zeros(total_stim,1);
+% Attack and Decay Reults
+attack_filter_matlab = zeros(total_stim,1);
+decay_filter_matlab  = zeros(total_stim,1);
+
 
 %% Calculate the Results
+decay_attack_tau  = 0;
+[decay_c1_a, decay_c2_a] = o1_lp_coeffs(decay_attack_tau,fs);
+
 for i = 1:1:total_stim
     addr_attack_matlab(i,1) = (band_num_input(i) - 1) * num_coeff;
     addr_decay_matlab(i,1)  = ((band_num_input(i) - 1) * num_coeff) + 1;
@@ -45,55 +54,73 @@ for i = 1:1:total_stim
     c2_a_matlab(i,1)        = 1 - c1_a_matlab(i,1);
     c1_d_matlab(i,1)        = ad_coeffs(addr_decay_matlab(i,1) + 1);   % Account for MATLAB Array Indexing starting at 1
     c2_d_matlab(i,1)        = 1 - c1_d_matlab(i,1);
+    if(i <= 8) % Initial Condition
+        attack_filter_matlab(i,1) = o1_ar_filter(data_input(i,1), c1_a_matlab(i,1), c2_a_matlab(i,1), c1_a_matlab(i,1), c2_a_matlab(i,1), buf_a(i,1));
+        decay_filter_matlab(i,1)  = o1_ar_filter(attack_filter_matlab(i,1), decay_c1_a, decay_c2_a, c1_d_matlab(i,1),  c2_d_matlab(i,1), buf_d(i,1));
+    else
+        attack_filter_matlab(i,1) = o1_ar_filter(data_input(i,1), c1_a_matlab(i,1), c2_a_matlab(i,1), c1_a_matlab(i,1), c2_a_matlab(i,1), attack_filter_matlab(i-num_bands,1));
+        decay_filter_matlab(i,1)  = o1_ar_filter(attack_filter_matlab(i,1), decay_c1_a, decay_c2_a, c1_d_matlab(i,1),  c2_d_matlab(i,1), decay_filter_matlab(i-num_bands,1));
+    end
 end
 
 %% Plot the Results
-% There is a single unit delay caused by reading from the Dual Port RAM
-% This is accounted for with the 2:end plotting of the outputs
 figure
-subplot(7,1,1)
-plot(band_num_input)
-hold on
-plot(Avalon_Sink_Data(2:end),'--')
-legend('Band Number Input', 'Band Number Output')
-title('Band Number')
+subplot(3,1,1)
+plot(data_input)
+legend("Audio Input Wave")
+title("Audio Input")
 
-subplot(7,1,2)
+subplot(3,1,2)
+plot(attack_filter_matlab)
+hold on
+plot(attack_filter_sim,'--')
+legend('MATLAB Code','Simulink')
+title('Attack Filter Simulation')
+
+subplot(3,1,3)
+plot(decay_filter_matlab)
+hold on
+plot(delay_filter_sim,'--')
+legend('MATLAB Code','Simulink')
+title('Decay Filter Simulation')
+
+figure
+subplot(6,1,1)
 plot(addr_attack_matlab)
 hold on
 plot(addr_attack_sim_out(1:end-1),'--')
 legend('MATLAB Code','Simulink')
 title('Attack Address Simulation')
 
-subplot(7,1,3)
+subplot(6,1,2)
 plot(addr_decay_matlab)
 hold on
 plot(addr_decay_sim_out(1:end-1),'--')
 legend('MATLAB Code','Simulink')
 title('Decay Address Simulation')
 
-subplot(7,1,4)
+subplot(6,1,3)
 plot(c1_a_matlab)
 hold on
 plot(c1_a_sim_out(2:end),'--')
 legend('MATLAB Code','Simulink')
 title('C1 Attack Cofficient Read Simulation')
 
-subplot(7,1,5)
+subplot(6,1,4)
 plot(c2_a_matlab)
 hold on
 plot(c2_a_sim_out(2:end),'--')
 legend('MATLAB Code','Simulink')
 title('C2 Attack Cofficient Read Simulation')
 
-subplot(7,1,6)
+subplot(6,1,5)
 plot(c1_d_matlab)
 hold on
 plot(c1_d_sim_out(2:end),'--')
 legend('MATLAB Code','Simulink')
 title('C1 Decay Cofficient Read Simulation')
 
-subplot(7,1,7)
+subplot(6,1,6)
 plot(c2_d_matlab)
 hold on
 plot(c2_d_sim_out(2:end),'--')
