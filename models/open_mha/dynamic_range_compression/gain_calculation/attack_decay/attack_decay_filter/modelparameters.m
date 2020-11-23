bb9d49eaf85204722f4f0f7b76f5edae46052eb5
Connor Dack <dack@flatearthinc.com>
2020-11-23 11:43:15 -0700

    Updated to match naming convention

--- models/open_mha/dynamic_range_compression/gain_calculation/attack_decay/attack_decay_filter/modelparameters.m
+++ models/open_mha/dynamic_range_compression/gain_calculation/attack_decay/attack_decay_filter/modelparameters.m
@@ -33,7 +33,7 @@ mp.F_bits = 23;
 Fs = 48e3;
 num_bands = 1;
 buf_a = 65; % Initial Condition of the Attack Filter Delay Block
-buf_r = 65; % This should be updated to buf_d to match naming convention
+buf_d = 65; % Initial Condition of the Delay Filter Delay Block
 
 %% Attack Filter Parameters
 % The Attack Filter is an "o1flt_lowpass_t" object - Line 105 of dc.hh
