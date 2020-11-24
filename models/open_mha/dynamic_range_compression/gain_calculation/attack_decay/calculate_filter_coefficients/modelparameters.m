d0d37e887a576fc6321cdb908eeac75dee92ba23
Connor Dack <dack@flatearthinc.com>
2020-11-24 15:10:33 -0700

    Updated to use dynamic data type input
    Note: Using double as the input fails in the dual port ram block

--- models/open_mha/dynamic_range_compression/gain_calculation/attack_decay/calculate_filter_coefficients/modelparameters.m
+++ models/open_mha/dynamic_range_compression/gain_calculation/attack_decay/calculate_filter_coefficients/modelparameters.m
@@ -40,20 +40,41 @@ mp.useAvalonInterface = false;
 mp.W_bits = 24;
 mp.F_bits = 23;
 
+%% Simulation Type - Either 'double' or 'fxpt'
+sim_type    = 'fxpt';                  
+
+% Attack and Decay Coefficient Fixed Point Paramters
+ad_coeff_fp_size = 16; % Word Size
+ad_coeff_fp_dec  = 16; % Fractional Bits
+ad_coeff_fp_sign = 0;  % Unsigned = 0, Signed = 1
+
+% Data Input/Feedback Paramters
+in_fp_size = 40; % Word Size
+in_fp_dec  = 32; % Fractional Bits
+in_fp_sign = 1;  % Unsigned = 0, Signed = 1
+
+%% Simulation Input for N Bands
+% Define the Input Data Types
+if(strcmp(sim_type,'double'))
+    input_type    = 'double';
+    ad_coeff_type = 'double';
+elseif(strcmp(sim_type,'fxpt'))
+    input_type    = fixdt(in_fp_sign,in_fp_size,in_fp_dec);
+    ad_coeff_type = fixdt(ad_coeff_fp_sign,ad_coeff_fp_size,ad_coeff_fp_dec);
+end
+
 %% Attack and Decay DPRAM Parameters
 % Create tau values to generate C1 Coefficients between 0 and 1
 tau_as = -1 ./ (log([0:1:num_bands-1] ./ num_bands) * fs);
 tau_ds = -1 ./ (log([num_bands-1:-1:0] ./ num_bands) * fs);
 % Initialization
-ad_coeffs = fi(zeros(2^coeff_size,1),0,16,16);
+ad_coeffs = zeros(2^coeff_size,1);
 
 % Create the Coefficient Array
 z = 1;
 for i = 1:2:2*num_bands-1
-    [attack_c1, ~]   = o1_lp_coeffs(tau_as(z), fs);
-    ad_coeffs(i,1)   = fi(attack_c1,0,16,16);
-    [decay_c1, ~]    = o1_lp_coeffs(tau_ds(z), fs);
-    ad_coeffs(i+1,1) = fi(decay_c1,0,16,16);
+    [ad_coeffs(i,1), ~]   = o1_lp_coeffs(tau_as(z), fs);
+    [ad_coeffs(i+1,1), ~] = o1_lp_coeffs(tau_ds(z), fs);
     z = z+1;
 end
 time = (0:1:length(band_num_input)-1) *1/fs;
