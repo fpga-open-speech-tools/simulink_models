cb607a7dca0dc0eaad226ad69afca661758c90e1
Connor Dack <dack@flatearthinc.com>
2020-11-24 12:21:52 -0700

    Updated for multiband simulation
    Updated the model parameters to:
     - Define the input data types
     - Use a variable number of frequency bands to calcuate the inputs and coefficients
    Updated the verifySim to compute the results for a variable number of bands and to plot the results
    Updated the simulation and model referencing block to use the defined input types

--- models/open_mha/dynamic_range_compression/gain_calculation/attack_decay/attack_decay_filter/modelparameters.m
+++ models/open_mha/dynamic_range_compression/gain_calculation/attack_decay/attack_decay_filter/modelparameters.m
@@ -30,102 +30,142 @@ mp.W_bits = 24;
 mp.F_bits = 23;
 
 %% Open MHA Parameters
-Fs          = 48e3;
-num_bands   = 1;
+fs          = 48e3;                    % Sampling Frequency
+num_bands   = 2;                       % Number of Frequency Bands to Simulate
 buf_a       = 65 .* ones(num_bands,1); % Initial Condition of the Attack Filter Delay Block
 buf_d       = 65 .* ones(num_bands,1); % Initial Condition of the Delay Filter Delay Block
-stim_length = AudioSource.fromFile(mp.testFile, mp.Fs, mp.nSamples).nSamples;
-time        = (0:1:stim_length-1)/fs;
+
+% Simulation Type - Either 'double' or 'fxpt'
+sim_type    = 'double';                  
+
+%% Fixed Point Sizes
+% Coefficient Paramters
+coeff_fp_size = 16; % Word Size
+coeff_fp_dec  = 16; % Fractional Bits
+coeff_fp_sign = 0;  % Unsigned = 0, Signed = 1
+
+% Data Input/Feedback Paramters
+in_fp_size = 40; % Word Size
+in_fp_dec  = 32; % Fractional Bits
+in_fp_sign = 1;  % Unsigned = 0, Signed = 1
+
+%% Simulation Input for N Bands
+% Define the Input Data Types
+if(strcmp(sim_type,'double'))
+    input_type = 'double';
+    coeff_type = 'double';
+elseif(strcmp(sim_type,'fxpt'))
+    input_type = fixdt(in_fp_sign,in_fp_size,in_fp_dec);
+    coeff_type = fixdt(coeff_fp_sign,coeff_fp_size,coeff_fp_dec);
+end
+
+% Initialization 
+audio_input       = AudioSource.fromFile(mp.testFile, mp.Fs, mp.nSamples);  % Read in the audio file
+audio_length      = audio_input.nSamples;                                   % Determine the number of points in the audio signal
+data_input_matrix = zeros(audio_length, num_bands);                         % Define the input matrix as the audio signal length X the number of bands
+data_input        = zeros(audio_input.nSamples * num_bands,1);              % Define the simulation input array 
+stim_length       = length(data_input);                                     % Deteremine the simulation length
+time              = (0:1:stim_length-1)/fs;                                 % Create the time vector for the simulation
+simulation_time   = (stim_length-1)/fs;                                     % Compute the simulation end point
+
+% Create the input signal
+% Copy the audio input wave into each column of the data input matrix
+for i = 1:num_bands
+    data_input_matrix(:,i) = audio_input.audio(:,1);                        
+end
+
+% Create a single data input array by interleafing the columns
+for i = 1:audio_length
+    for j = 1:num_bands
+        data_input(((i-1)*num_bands) + j,1) = data_input_matrix(i,j); 
+    end
+end
+data_input_ts = timeseries(data_input, time); % Convert the data array to a time series
 
 %% Attack Filter Parameters
 % The Attack Filter is an "o1flt_lowpass_t" object - Line 105 of dc.hh
 % Set the attack and release time constants equal for the attack filter
 % Lines 448-452 of mha_filter.cpp
-if num_bands == 1
-    attack_attack_tau  = 0.005;             % Time Constant in seconds - from openMHA Dynamic Compression Plugin documentation
-    attack_release_tau = attack_attack_tau; % Time Constant in seconds
-    [attack_c1_a, attack_c2_a] = o1_lp_coeffs(attack_attack_tau,Fs);
-    [attack_c1_r, attack_c2_r] = o1_lp_coeffs(attack_release_tau,Fs);
-else
-    attack_c1_a_array = zeros(num_bands,1);
-    attack_c2_a_array = zeros(num_bands,1);
-    attack_c1_r_array = zeros(num_bands,1);
-    attack_c2_r_array = zeros(num_bands,1);
-    for i = 1:num_bands
-        attack_attack_tau  = rand(1,1);         
-        attack_release_tau = attack_attack_tau;
-        [attack_c1_a_array(i,1), attack_c2_a_array(i,1)] = o1_lp_coeffs(attack_attack_tau,Fs);
-        [attack_c1_r_array(i,1), attack_c2_r_array(i,1)] = o1_lp_coeffs(attack_release_tau,Fs);
+
+% Initialize the attack coefficient arrays: 1 coefficient per band
+attack_c1_a_array = zeros(num_bands,1);
+attack_c2_a_array = zeros(num_bands,1);
+attack_c1_r_array = zeros(num_bands,1);
+attack_c2_r_array = zeros(num_bands,1);
+
+
+% Initialize the attack coefficient input arrays
+attack_c1_a_in = zeros(stim_length,1);
+attack_c2_a_in = zeros(stim_length,1);
+attack_c1_r_in = zeros(stim_length,1);
+attack_c2_r_in = zeros(stim_length,1);
+
+for j = 1:num_bands
+    if j == 1
+        attack_attack_tau  = 0.005; % Defined in the Open MHA Plug in documnetation
+    else
+        attack_attack_tau  = attack_attack_tau /2; % Reduce the time constant by half for each consecutive band 
     end
+    attack_release_tau = attack_attack_tau; % Set the release time constant equal to the attack time constant - Lines 450-451 of mha_filter.cpp
+    [attack_c1_a_array(j,1), attack_c2_a_array(j,1)] = o1_lp_coeffs(attack_attack_tau,fs);  % Compute the attack coefficients - Lines 589-599 of mha_filter.cpp
+    [attack_c1_r_array(j,1), attack_c2_r_array(j,1)] = o1_lp_coeffs(attack_release_tau,fs); % Compute the release coefficients - Lines 589-599 of mha_filter.cpp
 end
 
-attack_c1_a_double = zeros(stim_length,1);
-attack_c2_a_double = zeros(stim_length,1);
-attack_c1_r_double = zeros(stim_length,1);
-attack_c2_r_double = zeros(stim_length,1);
-if num_bands == 1
-    for i = 1:stim_length
-        attack_c1_a_double(i,1) = attack_c1_a;
-        attack_c2_a_double(i,1) = attack_c2_a;
-        attack_c1_r_double(i,1) = attack_c1_r;
-        attack_c2_r_double(i,1) = attack_c2_r;
-    end
-else
-    for i = 1:stim_length
-        attack_c1_a_double(i,1) = attack_c1_a_array(mod(i,num_bands) + 1,1);
-        attack_c2_a_double(i,1) = attack_c2_a_array(mod(i,num_bands) + 1,1);
-        attack_c1_r_double(i,1) = attack_c1_r_array(mod(i,num_bands) + 1,1);
-        attack_c2_r_double(i,1) = attack_c2_r_array(mod(i,num_bands) + 1,1);
+% Create the coefficient input array 
+% The coefficients are repeated [1:num_bands 1:num_bands]
+for i = 1:audio_length
+    for j = 1:num_bands
+        attack_c1_a_in(((i-1)*num_bands) + j,1) = attack_c1_a_array(j,1);
+        attack_c2_a_in(((i-1)*num_bands) + j,1) = attack_c2_a_array(j,1);
+        attack_c1_r_in(((i-1)*num_bands) + j,1) = attack_c1_r_array(j,1);
+        attack_c2_r_in(((i-1)*num_bands) + j,1) = attack_c2_r_array(j,1);
     end
 end
-attack_c1_a_ts = timeseries(attack_c1_a_double,time);
-attack_c2_a_ts = timeseries(attack_c2_a_double,time);
-attack_c1_r_ts = timeseries(attack_c1_r_double,time);
-attack_c2_r_ts = timeseries(attack_c2_r_double,time);
+% Convert the Attack Coefficient Array to a Timeseries
+attack_c1_a_ts = timeseries(attack_c1_a_in,time);
+attack_c2_a_ts = timeseries(attack_c2_a_in,time);
+attack_c1_r_ts = timeseries(attack_c1_r_in,time);
+attack_c2_r_ts = timeseries(attack_c2_r_in,time);
 
 %% Decay Filter Parameters
 % The Decay Filter is an "o1flt_maxtrack_t" object - Line 106 of dc.hh
 % Set the attack time constant = 0 - Line 507 of mha_filter.cpp
 % Set the release time consant = milliseconds - Line 508 of mha_filter.cpp
+decay_c1_a_array = zeros(num_bands,1);
+decay_c2_a_array = zeros(num_bands,1);
+decay_c1_r_array = zeros(num_bands,1);
+decay_c2_r_array = zeros(num_bands,1);
 
-decay_attack_tau  = 0;
-if num_bands == 1
-    decay_release_tau = 0.060; % Time Constant in seconds - from openMHA Dynamic Compression Plugin documentation:
-    [decay_c1_a, decay_c2_a] = o1_lp_coeffs(decay_attack_tau,Fs);
-    [decay_c1_r, decay_c2_r] = o1_lp_coeffs(decay_release_tau,Fs);
-else
-    decay_c1_a_array = zeros(num_bands,1);
-    decay_c2_a_array = zeros(num_bands,1);
-    decay_c1_r_array = zeros(num_bands,1);
-    decay_c2_r_array = zeros(num_bands,1);
-    for i = 1:num_bands        
-        decay_release_tau = rand(1,1); 
-        [decay_c1_a_array(i,1), decay_c2_a_array(i,1)] = o1_lp_coeffs(decay_attack_tau,Fs);
-        [decay_c1_r_array(i,1), decay_c2_r_array(i,1)] = o1_lp_coeffs(attack_release_tau,Fs);
+decay_c1_a_fp = zeros(stim_length,1);
+decay_c2_a_fp = zeros(stim_length,1);
+decay_c1_r_fp = zeros(stim_length,1);
+decay_c2_r_fp = zeros(stim_length,1);
+
+decay_attack_tau  = 0; % Line 507 of mha_filter.cpp
+for j = 1:num_bands        
+    if j == 1
+        decay_release_tau = 0.060; % Defined in the Open MHA Plug in documnetation
+    else
+        decay_release_tau = decay_release_tau / 2; % Reduce the time constant by half for each consecutive band 
     end
+
+    [decay_c1_a_array(j,1), decay_c2_a_array(j,1)] = o1_lp_coeffs(decay_attack_tau,fs);  % Compute the attack coefficients - Lines 589-599 of mha_filter.cpp
+    [decay_c1_r_array(j,1), decay_c2_r_array(j,1)] = o1_lp_coeffs(decay_release_tau,fs); % Compute the release coefficients - Lines 589-599 of mha_filter.cpp
 end
 
-decay_c1_a_double = zeros(stim_length,1);
-decay_c2_a_double = zeros(stim_length,1);
-decay_c1_r_double = zeros(stim_length,1);
-decay_c2_r_double = zeros(stim_length,1);
-if num_bands == 1
-    for i = 1:stim_length
-        decay_c1_a_double(i,1) = decay_c1_a;
-        decay_c2_a_double(i,1) = decay_c2_a;
-        decay_c1_r_double(i,1) = decay_c1_r;
-        decay_c2_r_double(i,1) = decay_c2_r;
-    end
-else
-    for i = 1:stim_length
-        decay_c1_a_double(i,1) = decay_c1_a_array(mod(i,num_bands) + 1,1);
-        decay_c2_a_double(i,1) = decay_c2_a_array(mod(i,num_bands) + 1,1);
-        decay_c1_r_double(i,1) = decay_c1_r_array(mod(i,num_bands) + 1,1);
-        decay_c2_r_double(i,1) = decay_c2_r_array(mod(i,num_bands) + 1,1);
+% Create the coefficient input array 
+% The coefficients are repeated [1:num_bands 1:num_bands]
+for i = 1:audio_length
+    for j = 1:num_bands
+        decay_c1_a_fp(((i-1)*num_bands) + j,1) = decay_c1_a_array(j,1);
+        decay_c2_a_fp(((i-1)*num_bands) + j,1) = decay_c2_a_array(j,1);
+        decay_c1_r_fp(((i-1)*num_bands) + j,1) = decay_c1_r_array(j,1);
+        decay_c2_r_fp(((i-1)*num_bands) + j,1) = decay_c2_r_array(j,1);
     end
 end
-decay_c1_a_ts = timeseries(decay_c1_a_double,time);
-decay_c2_a_ts = timeseries(decay_c2_a_double,time);
-decay_c1_r_ts = timeseries(decay_c1_r_double,time);
-decay_c2_r_ts = timeseries(decay_c2_r_double,time);
+% Convert the Attack Coefficient Array to a Timeseries
+decay_c1_a_ts = timeseries(decay_c1_a_fp,time);
+decay_c2_a_ts = timeseries(decay_c2_a_fp,time);
+decay_c1_r_ts = timeseries(decay_c1_r_fp,time);
+decay_c2_r_ts = timeseries(decay_c2_r_fp,time);
 
--- models/open_mha/dynamic_range_compression/gain_calculation/attack_decay/attack_decay_filter/verifySim.m
+++ models/open_mha/dynamic_range_compression/gain_calculation/attack_decay/attack_decay_filter/verifySim.m
@@ -29,41 +29,180 @@
 
 %% Initialization
 close all;
-data_input = testSignal.audio(:,1);
-total_stim = length(data_input);
+attack_buff_matlab       = zeros(audio_length,num_bands);
+attack_buff_sim_matrix   = zeros(audio_length,num_bands);
+attack_filter_matlab     = zeros(audio_length,num_bands);
+attack_filter_sim_matrix = zeros(audio_length,num_bands);
+
+delay_buff_matlab        = zeros(audio_length,num_bands);
+delay_buff_sim_matrix    = zeros(audio_length,num_bands);
+decay_filter_matlab      = zeros(audio_length,num_bands);
+delay_filter_sim_matrix  = zeros(audio_length,num_bands);
 
 %% Calculate the Results
-attack_filter_matlab = zeros(total_stim,1);
-decay_filter_matlab  = zeros(total_stim,1);
-
-for i =1:1:total_stim
-    % Based on Line 282 from dc.cpp
-    if(i <= num_bands) % Initial Condition
-        attack_filter_matlab(i,1) = o1_ar_filter(data_input(i,1), attack_c1_a_double(i,1), attack_c2_a_double(i,1), attack_c1_r_double(i,1), attack_c2_r_double(i,1), buf_a(i,1));
-        decay_filter_matlab(i,1)  = o1_ar_filter(attack_filter_matlab(i,1), decay_c1_a_double(i,1), decay_c2_a_double(i,1), decay_c1_r_double(i,1), decay_c2_r_double(i,1), buf_d(i,1));
-    else
-        attack_filter_matlab(i,1) = o1_ar_filter(data_input(i,1), attack_c1_a_double(i,1), attack_c2_a_double(i,1), attack_c1_r_double(i,1), attack_c2_r_double(i,1), attack_filter_matlab(i-num_bands,1));
-        decay_filter_matlab(i,1)  = o1_ar_filter(attack_filter_matlab(i,1), decay_c1_a_double(i,1), decay_c2_a_double(i,1), decay_c1_r_double(i,1), decay_c2_r_double(i,1), decay_filter_matlab(i-num_bands,1));
+% Based on Line 282 from dc.cpp
+for i = 1:1:audio_length
+    for j = 1:1:num_bands
+        if(i == 1) % Initial Condition
+            attack_buff_matlab(i,j)   = buf_a(j,1);
+            attack_filter_matlab(i,j) = o1_ar_filter(data_input_matrix(i,j), attack_c1_a_array(j,1), attack_c2_a_array(j,1), attack_c1_r_array(j,1), attack_c2_r_array(j,1), attack_buff_matlab(i,j));
+            
+            delay_buff_matlab(i,j)    = buf_d(j,1);
+            decay_filter_matlab(i,j)  = o1_ar_filter(attack_filter_matlab(i,j), decay_c1_a_array(j,1), decay_c2_a_array(j,1), decay_c1_r_array(j,1), decay_c2_r_array(j,1), delay_buff_matlab(i,j));
+        else
+            attack_buff_matlab(i,j)   = attack_filter_matlab(i-1,j);
+            attack_filter_matlab(i,j) = o1_ar_filter(data_input_matrix(i,j), attack_c1_a_array(j,1), attack_c2_a_array(j,1), attack_c1_r_array(j,1), attack_c2_r_array(j,1), attack_buff_matlab(i,j));
+            
+            delay_buff_matlab(i,j)    = decay_filter_matlab(i-1,j);
+            decay_filter_matlab(i,j)  = o1_ar_filter(attack_filter_matlab(i,j), decay_c1_a_array(j,1), decay_c2_a_array(j,1), decay_c1_r_array(j,1), decay_c2_r_array(j,1), delay_buff_matlab(i,j));
+        end
     end
 end
 
+%% Parse the Simulink Results
+for i = 1:audio_length
+    for j = 1:num_bands
+        attack_buff_sim_matrix(i,j)   = attack_buff_sim(((i-1)*num_bands) + j,1);
+        attack_filter_sim_matrix(i,j) = attack_filter_sim(((i-1)*num_bands) + j,1);
+        
+        delay_buff_sim_matrix(i,j)    = decay_buff_sim(((i-1)*num_bands) + j,1);
+        delay_filter_sim_matrix(i,j)  = delay_filter_sim(((i-1)*num_bands) + j,1);
+    end    
+end
+
 %% Plot the Results
-figure
-subplot(3,1,1)
-plot(data_input)
-legend("Audio Input Wave")
-title("Audio Input")
+if num_bands == 1
+    figure
+    plot(data_input(:,1))
+    legend('Audio Input Wave')
+    title(['Number of Bands = ' num2str(num_bands)])
+    
+    figure
+    subplot(4,1,1)
+    plot(attack_buff_matlab(:,1))
+    hold on
+    plot(attack_buff_sim_matrix(:,1),'--')
+    legend('MATLAB Code','Simulink')
+    title('Attack Buffer Simulation')
+    
+    subplot(4,1,2)
+    plot(attack_filter_matlab(:,1))
+    hold on
+    plot(attack_filter_sim_matrix(:,1),'--')
+    legend('MATLAB Code','Simulink')
+    title('Attack Filter Simulation')
+    
+    subplot(4,1,3)
+    plot(delay_buff_matlab(:,1))
+    hold on
+    plot(delay_buff_sim_matrix(:,1),'--')
+    legend('MATLAB Code','Simulink')
+    title('Decay Buffer Simulation')
 
-subplot(3,1,2)
-plot(attack_filter_matlab)
-hold on
-plot(attack_filter_sim,'--')
-legend('MATLAB Code','Simulink')
-title('Attack Filter Simulation')
+    subplot(4,1,4)
+    plot(decay_filter_matlab(:,1))
+    hold on
+    plot(delay_filter_sim_matrix(:,1),'--')
+    legend('MATLAB Code','Simulink')
+    title('Decay Filter Simulation')
+elseif num_bands == 2
+    figure
+    plot(data_input(:,1))
+    legend('Audio Input Wave')
+    title(['Number of Bands = ' num2str(num_bands)])
+    
+    figure
+    subplot(4,1,1)
+    plot(attack_buff_matlab(:,1))
+    hold on
+    plot(attack_buff_sim_matrix(:,1),'--')
+    legend('MATLAB Code','Simulink')
+    title('Attack Buffer Simulation - Band 1')
+    
+    subplot(4,1,2)
+    plot(attack_filter_matlab(:,1))
+    hold on
+    plot(attack_filter_sim_matrix(:,1),'--')
+    legend('MATLAB Code','Simulink')
+    title('Attack Filter Simulation - Band 1')
+    
+    subplot(4,1,3)
+    plot(delay_buff_matlab(:,1))
+    hold on
+    plot(delay_buff_sim_matrix(:,1),'--')
+    legend('MATLAB Code','Simulink')
+    title('Decay Buffer Simulation - Band 1')
 
-subplot(3,1,3)
-plot(decay_filter_matlab)
-hold on
-plot(delay_filter_sim,'--')
-legend('MATLAB Code','Simulink')
-title('Decay Filter Simulation')
+    subplot(4,1,4)
+    plot(decay_filter_matlab(:,1))
+    hold on
+    plot(delay_filter_sim_matrix(:,1),'--')
+    legend('MATLAB Code','Simulink')
+    title('Decay Filter Simulation - Band 1')
+        
+    figure
+    subplot(4,1,1)
+    plot(attack_buff_matlab(:,2))
+    hold on
+    plot(attack_buff_sim_matrix(:,2),'--')
+    legend('MATLAB Code','Simulink')
+    title('Attack Buffer Simulation - Band 2')
+    
+    subplot(4,1,2)
+    plot(attack_filter_matlab(:,2))
+    hold on
+    plot(attack_filter_sim_matrix(:,2),'--')
+    legend('MATLAB Code','Simulink')
+    title('Attack Filter Simulation - Band 2')
+    
+    subplot(4,1,3)
+    plot(delay_buff_matlab(:,2))
+    hold on
+    plot(delay_buff_sim_matrix(:,2),'--')
+    legend('MATLAB Code','Simulink')
+    title('Decay Buffer Simulation - Band 2')
+
+    subplot(4,1,4)
+    plot(decay_filter_matlab(:,2))
+    hold on
+    plot(delay_filter_sim_matrix(:,2),'--')
+    legend('MATLAB Code','Simulink')
+    title('Decay Filter Simulation - Band 2')
+elseif num_bands == 8
+    figure
+    subplot(5,1,1)
+    plot(data_input(:,1))
+    legend('Audio Input Wave')
+    title(['Number of Bands = ' num2str(num_bands)])
+
+    subplot(5,1,2)
+    plot(attack_filter_matlab(:,1))
+    hold on
+    plot(attack_filter_sim_matrix(:,1),'--')
+    legend('MATLAB Code','Simulink')
+    title('Attack Filter Simulation - Band 1')
+
+    subplot(5,1,3)
+    plot(decay_filter_matlab(:,1))
+    hold on
+    plot(delay_filter_sim_matrix(:,1),'--')
+    legend('MATLAB Code','Simulink')
+    title('Decay Filter Simulation - Band 1')
+    
+    subplot(5,1,4)
+    plot(attack_filter_matlab(:,8))
+    hold on
+    plot(attack_filter_sim_matrix(:,8),'--')
+    legend('MATLAB Code','Simulink')
+    title('Attack Filter Simulation - Band 8')
+
+    subplot(5,1,5)
+    plot(decay_filter_matlab(:,8))
+    hold on
+    plot(delay_filter_sim_matrix(:,8),'--')
+    legend('MATLAB Code','Simulink')
+    title('Decay Filter Simulation - Band 8')
+else
+    msg = [num2str(num_bands) ' Bands not supported for plotting'];
+    f = warndlg(msg,'Warning'); 
+end
