2d8312f437ec4748f6743733cbd00e7b528e1bb0
Connor Dack <dack@flatearthinc.com>
2020-12-04 13:36:04 -0700

    Initial Commit

---
+++ models/open_mha/dynamic_range_compression/dynamic_range_compression_sim_stop.m
@@ -0,0 +1,25 @@
+% dynamic_range_compression_init.m  
+%
+% Copyright 2020 AudioLogic, Inc
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+% INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+% PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+% FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+% ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+%
+% Matthew Blunt, Connor Dack
+% AudioLogic, Inc
+% 985 Technology Blvd
+% Bozeman, MT 59718
+% openspeech@flatearthinc.com
+
+%% Plot the Results
+figure;
+subplot(2,1,1)
+plot(modified_FFT_data_real)
+title('FFT Data: Real')
+
+subplot(2,1,2)
+plot(modified_FFT_data_imag)
+title('FFT Data: Imaginary')
