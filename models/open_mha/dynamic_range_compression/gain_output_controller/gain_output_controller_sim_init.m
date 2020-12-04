8f668e26c780aea7bbd4ee7a14102b7234ec0d31
Connor Dack <dack@flatearthinc.com>
2020-12-04 13:10:04 -0700

    Updated the simulation to use the results from the Gain Calculation
    Updated the model parameters
    Added Plottoing of the gain output

--- models/open_mha/dynamic_range_compression/gain_output_controller/gain_output_controller_sim_init.m
+++ models/open_mha/dynamic_range_compression/gain_output_controller/gain_output_controller_sim_init.m
@@ -6,30 +6,54 @@
 % FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 % ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 %
-% Matthew Blunt
-% Audio Logic
+% Connor Dack
+% AudioLogic, Inc
 % 985 Technology Blvd
 % Bozeman, MT 59718
 % openspeech@flatearthinc.com
 
-% openMHA Dynamic Compression Simulink Model Code
-% Gain Application Controller Test/Verification Simulation Init Script
-% 11/18/2020
+%% Initialization
+load gain_calculation_simulation.mat         % Results from the Gain Calcualtion Block
+addpath(genpath('..\referenced_functions')); % Frost Library
 
-% clear all;
-% close all;
+%% Open MHA Parameters 
+audio_fs           = 48e3;                          % Audio Sampling Rate from the AD1939
+fft_up_sample_rate = 128;                           % FFT Up Sample Rate
+fs                 = audio_fs * fft_up_sample_rate; % Samplig Frequency
+stop_time          = gc_gain_sim.Time(end);
 
-%% NOTES
+%% Simulation Type - Either 'double' or 'fxpt'
+sim_type = 'fxpt';                  
 
-% The following script is designed as an init script for the Gain
-% Application Controller Simulink model. It sets the parameters for both 
-% the Simulink model and the comparison MATLAB computation. In addition, it 
-% provides the test signals for the Simulink Model. Note that there is no
-% exact openMHA source code to compare against here, as the gain
-% application controller exists due to the time-division multiplexing
-% resulting from the data flow architecture in the Simulink model.
+% Gain - Fixed Point Paramters
+gain_coeff_fp_size = 16; % Word Size
+gain_coeff_fp_dec  = 8;  % Fractional Bits
+gain_coeff_fp_sign = 1;  % Unsigned = 0, Signed = 1
 
-%% Model Parameters
+% Define the Input Data Types
+if(strcmp(sim_type,'double'))
+    gain_coeff_type = 'double';
+elseif(strcmp(sim_type,'fxpt'))
+    gain_coeff_type = fixdt(gain_coeff_fp_sign,gain_coeff_fp_size,gain_coeff_fp_dec);
+end
 
+%% Gain Output Controller Parameters
+% FFT Parameters
+FFTsize = 256;
+num_bins = FFTsize/2 + 1;
+freq = linspace(0,24000,129);
+binwidth = (audio_fs/2)/(FFTsize/2);
 
-%% Simulation Input Signals
+num_bands = 8;
+ef = [0 250 500 750 1000 2000 4000 12000 24000];
+band_sizes = calculate_band_sizes(ef, num_bins, binwidth, num_bands);
+
+% Gain Output Dual Port Memory Parameters
+gainapp_dp_memory = log2(num_bands) + 1;
+gain_delay = 6;
+accum_delay = max(band_sizes);
+system_delay = gain_delay + accum_delay;
+accum_delay_memory = ceil(log2(accum_delay));
+system_delay_memory = ceil(log2(system_delay));
+delay_memory_size =2^system_delay_memory;
+accum_delay_memory_size = 2^accum_delay_memory;
--- models/open_mha/dynamic_range_compression/gain_output_controller/gain_output_controller_sim_stop.m
+++ models/open_mha/dynamic_range_compression/gain_output_controller/gain_output_controller_sim_stop.m
@@ -6,29 +6,15 @@
 % FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 % ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 %
-% Matthew Blunt
-% Audio Logic
+% Connor Dack
+% AudioLogic, Inc
 % 985 Technology Blvd
 % Bozeman, MT 59718
 % openspeech@flatearthinc.com
 
-% openMHA Dynamic Compression Simulink Model Code
-% Gain Application Controller Test/Verification Simulation Stop Script
-% 11/18/2020
-
-%% NOTES
-
-% The following script is designed to isolate and test the Gain Application
-% Controller simulation results against expected results. Note that these
-% results will not be derived directly from openMHA source code, as the
-% gain application controller exists due to the time-division multiplexing
-% resulting from the data flow architecture in the Simulink model.
-
-%% Calculate openMHA Results
-
-
-%% Compare Results
-
 
 %% Plot Results
-
+figure;
+plot(goc_gain_sim.Data)
+legend('Gain')
+title('Gain Output Controller Simulation')
