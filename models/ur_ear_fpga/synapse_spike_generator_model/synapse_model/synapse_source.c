6054ca3573732860cf1a686f32897a64fbd23026
Tyler Davis <davis@flatearthinc.com>
2020-12-07 10:41:34 -0700

    Added catch to prevent attempting to write beyond the syn array

--- models/ur_ear_fpga/synapse_spike_generator_model/synapse_model/synapse_source.c
+++ models/ur_ear_fpga/synapse_spike_generator_model/synapse_model/synapse_source.c
@@ -113,10 +113,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     
     /* Calculate number of samples for total repetition time */
     
-    totalstim = (int)floor(pxbins/nrep);    
-    //totalstim = totalstim * nrep; // Modified to match spikegen_source
-
-    mexPrintf("Calculated the total sim: %d\n",totalstim);
+    totalstim = totalstim * nrep; // Modified to match spikegen_source
     
     px = (double*)mxCalloc(totalstim*nrep,sizeof(double));
     
@@ -159,9 +156,7 @@ double Synapse(double *ihcout, double tdres, double cf, int totalstim, int nrep,
     double *sout1, *sout2, *synSampOut, *powerLawIn, *mappingOut, *TmpSyn;
     double *m1, *m2, *m3, *m4, *m5;
     double *n1, *n2, *n3;
-    
-    mexPrintf("Here0\n");
-    
+        
     mxArray	*randInputArray[5], *randOutputArray[1];
     double *randNums;
     
@@ -192,13 +187,11 @@ double Synapse(double *ihcout, double tdres, double cf, int totalstim, int nrep,
     alpha1 = 1.5e-6*100e3; beta1 = 5e-4; I1 = 0;
     alpha2 = 1e-2*100e3; beta2 = 1e-1; I2 = 0;
     
-    mexPrintf("Here\n");
     /*----------------------------------------------------------*/
     /*------- Generating a random sequence ---------------------*/
     /*----------------------------------------------------------*/
     randInputArray[0] = mxCreateDoubleMatrix(1, 1, mxREAL);
     *mxGetPr(randInputArray[0])= ceil((totalstim*nrep+2*delaypoint)*tdres*sampFreq);
-    mexPrintf("%f\n",ceil((totalstim*nrep+2*delaypoint)*tdres*sampFreq));
     randInputArray[1] = mxCreateDoubleMatrix(1, 1, mxREAL);
     *mxGetPr(randInputArray[1])= 1/sampFreq;
     randInputArray[2] = mxCreateDoubleMatrix(1, 1, mxREAL);
@@ -208,136 +201,134 @@ double Synapse(double *ihcout, double tdres, double cf, int totalstim, int nrep,
     randInputArray[4] = mxCreateDoubleMatrix(1, 1, mxREAL);
     *mxGetPr(randInputArray[4])= spont; /* high, medium, or low */
     
-    mexPrintf("Params: %d, %d, %d, %d, %d\n",randInputArray[0],randInputArray[1],randInputArray[2],randInputArray[3],randInputArray[4]);
-    
-    // mexCallMATLAB(1, randOutputArray, 5, randInputArray, "ffGn");
-    // randNums = mxGetPr(randOutputArray[0]);
-    // mexPrintf("Here2\n");
+    mexCallMATLAB(1, randOutputArray, 5, randInputArray, "ffGn");
+    randNums = mxGetPr(randOutputArray[0]);
     /*----------------------------------------------------------*/
     /*----- Mapping Function from IHCOUT to input to the PLA ----------------------*/
     /*----------------------------------------------------------*/
-    // cfslope = pow(spont,0.19)*pow(10,-0.87);
-    // cfconst = 0.1*pow(log10(spont),2)+0.56*log10(spont)-0.84;
-    // cfsat = pow(10,(cfslope*8965.5/1e3 + cfconst));
-    // cf_factor = __min(cfsat,pow(10,cfslope*cf/1e3 + cfconst))*2.0;
+    cfslope = pow(spont,0.19)*pow(10,-0.87);
+    cfconst = 0.1*pow(log10(spont),2)+0.56*log10(spont)-0.84;
+    cfsat = pow(10,(cfslope*8965.5/1e3 + cfconst));
+    cf_factor = __min(cfsat,pow(10,cfslope*cf/1e3 + cfconst))*2.0;
 
-    // multFac = __max(2.95*__max(1.0,1.5-spont/100),4.3-0.2*cf/1e3);
+    multFac = __max(2.95*__max(1.0,1.5-spont/100),4.3-0.2*cf/1e3);
 
-    // k = 0;
-    // for (indx=0; indx<totalstim*nrep; ++indx)
-    // {
-        // mappingOut[k] = pow(10,(0.9*log10(fabs(ihcout[indx])*cf_factor))+ multFac);
-        // if (ihcout[indx]<0) mappingOut[k] = - mappingOut[k];
-        // k=k+1;
-    // }
-    // for (k=0; k<delaypoint; k++)
-        // powerLawIn[k] = mappingOut[0]+3.0*spont;
-    // for (k=delaypoint; k<totalstim*nrep+delaypoint; k++)
-        // powerLawIn[k] = mappingOut[k-delaypoint]+3.0*spont;
-    // for (k=totalstim*nrep+delaypoint; k<totalstim*nrep+3*delaypoint; k++)
-        // powerLawIn[k] = powerLawIn[k-1]+3.0*spont;
-    // mexPrintf("Here3\n");
-    // /*----------------------------------------------------------*/
-    // /*------ Downsampling to sampFreq (Low) sampling rate ------*/
-    // /*----------------------------------------------------------*/
-    // IhcInputArray[0] = mxCreateDoubleMatrix(1, k, mxREAL);
-    // ihcDims = mxGetPr(IhcInputArray[0]);
-    // for (i=0;i<k;++i)
-        // ihcDims[i] = powerLawIn[i];
-    // IhcInputArray[1] = mxCreateDoubleMatrix(1, 1, mxREAL);
-    // *mxGetPr(IhcInputArray[1])= 1;
-    // IhcInputArray[2] = mxCreateDoubleMatrix(1, 1, mxREAL);
-    // *mxGetPr(IhcInputArray[2])= resamp;
-    // mexCallMATLAB(1, IhcOutputArray, 3, IhcInputArray, "resample");
-    // sampIHC = mxGetPr(IhcOutputArray[0]);
+    k = 0;
+    for (indx=0; indx<totalstim*nrep; ++indx)
+    {
+        mappingOut[k] = pow(10,(0.9*log10(fabs(ihcout[indx])*cf_factor))+ multFac);
+        if (ihcout[indx]<0) mappingOut[k] = - mappingOut[k];
+        k=k+1;
+    }
+    for (k=0; k<delaypoint; k++)
+        powerLawIn[k] = mappingOut[0]+3.0*spont;
+    for (k=delaypoint; k<totalstim*nrep+delaypoint; k++)
+        powerLawIn[k] = mappingOut[k-delaypoint]+3.0*spont;
+    for (k=totalstim*nrep+delaypoint; k<totalstim*nrep+3*delaypoint; k++)
+        powerLawIn[k] = powerLawIn[k-1]+3.0*spont;
+    /*----------------------------------------------------------*/
+    /*------ Downsampling to sampFreq (Low) sampling rate ------*/
+    /*----------------------------------------------------------*/
+    IhcInputArray[0] = mxCreateDoubleMatrix(1, k, mxREAL);
+    ihcDims = mxGetPr(IhcInputArray[0]);
+    for (i=0;i<k;++i)
+        ihcDims[i] = powerLawIn[i];
+    IhcInputArray[1] = mxCreateDoubleMatrix(1, 1, mxREAL);
+    *mxGetPr(IhcInputArray[1])= 1;
+    IhcInputArray[2] = mxCreateDoubleMatrix(1, 1, mxREAL);
+    *mxGetPr(IhcInputArray[2])= resamp;
+    mexCallMATLAB(1, IhcOutputArray, 3, IhcInputArray, "resample");
+    sampIHC = mxGetPr(IhcOutputArray[0]);
     
-    // mxFree(powerLawIn); mxFree(mappingOut);
-    // mexPrintf("Here4\n");
-    // /*----------------------------------------------------------*/
-    // /*----- Running Power-law Adaptation -----------------------*/
-    // /*----------------------------------------------------------*/
-    // k = 0;
-    // for (indx=0; indx<floor((totalstim*nrep+2*delaypoint)*tdres*sampFreq); indx++)
-    // {
-        // sout1[k]  = __max( 0, sampIHC[indx] + randNums[indx]- alpha1*I1);
-        // sout2[k]  = __max( 0, sampIHC[indx] - alpha2*I2);
+    mxFree(powerLawIn); mxFree(mappingOut);
+    /*----------------------------------------------------------*/
+    /*----- Running Power-law Adaptation -----------------------*/
+    /*----------------------------------------------------------*/
+    k = 0;
+    for (indx=0; indx<(int)floor((totalstim*nrep+2*delaypoint)*tdres*sampFreq); indx++)
+    {
+        sout1[k]  = __max( 0, sampIHC[indx] + randNums[indx]- alpha1*I1);
+        sout2[k]  = __max( 0, sampIHC[indx] - alpha2*I2);
         
-        // if (implnt==1)    /* ACTUAL Implementation */
-        // {
-            // I1 = 0; I2 = 0;
-            // for (j=0; j<k+1; ++j)
-            // {
-                // I1 += (sout1[j])*binwidth/((k-j)*binwidth + beta1);
-                // I2 += (sout2[j])*binwidth/((k-j)*binwidth + beta2);
-            // }
-        // } /* end of actual */
+        if (implnt==1)    /* ACTUAL Implementation */
+        {
+            I1 = 0; I2 = 0;
+            for (j=0; j<k+1; ++j)
+            {
+                I1 += (sout1[j])*binwidth/((k-j)*binwidth + beta1);
+                I2 += (sout2[j])*binwidth/((k-j)*binwidth + beta2);
+            }
+        } /* end of actual */
         
-        // if (implnt==0)    /* APPROXIMATE Implementation */
-        // {
-            // if (k==0)
-            // {
-                // n1[k] = 1.0e-3*sout2[k];
-                // n2[k] = n1[k]; n3[0]= n2[k];
-            // }
-            // else if (k==1)
-            // {
-                // n1[k] = 1.992127932802320*n1[k-1]+ 1.0e-3*(sout2[k] - 0.994466986569624*sout2[k-1]);
-                // n2[k] = 1.999195329360981*n2[k-1]+ n1[k] - 1.997855276593802*n1[k-1];
-                // n3[k] = -0.798261718183851*n3[k-1]+ n2[k] + 0.798261718184977*n2[k-1];
-            // }
-            // else
-            // {
-                // n1[k] = 1.992127932802320*n1[k-1] - 0.992140616993846*n1[k-2]+ 1.0e-3*(sout2[k] - 0.994466986569624*sout2[k-1] + 0.000000000002347*sout2[k-2]);
-                // n2[k] = 1.999195329360981*n2[k-1] - 0.999195402928777*n2[k-2]+n1[k] - 1.997855276593802*n1[k-1] + 0.997855827934345*n1[k-2];
-                // n3[k] =-0.798261718183851*n3[k-1] - 0.199131619873480*n3[k-2]+n2[k] + 0.798261718184977*n2[k-1] + 0.199131619874064*n2[k-2];
-            // }
-            // I2 = n3[k];
+        if (implnt==0)    /* APPROXIMATE Implementation */
+        {
+            if (k==0)
+            {
+                n1[k] = 1.0e-3*sout2[k];
+                n2[k] = n1[k]; n3[0]= n2[k];
+            }
+            else if (k==1)
+            {
+                n1[k] = 1.992127932802320*n1[k-1]+ 1.0e-3*(sout2[k] - 0.994466986569624*sout2[k-1]);
+                n2[k] = 1.999195329360981*n2[k-1]+ n1[k] - 1.997855276593802*n1[k-1];
+                n3[k] = -0.798261718183851*n3[k-1]+ n2[k] + 0.798261718184977*n2[k-1];
+            }
+            else
+            {
+                n1[k] = 1.992127932802320*n1[k-1] - 0.992140616993846*n1[k-2]+ 1.0e-3*(sout2[k] - 0.994466986569624*sout2[k-1] + 0.000000000002347*sout2[k-2]);
+                n2[k] = 1.999195329360981*n2[k-1] - 0.999195402928777*n2[k-2]+n1[k] - 1.997855276593802*n1[k-1] + 0.997855827934345*n1[k-2];
+                n3[k] =-0.798261718183851*n3[k-1] - 0.199131619873480*n3[k-2]+n2[k] + 0.798261718184977*n2[k-1] + 0.199131619874064*n2[k-2];
+            }
+            I2 = n3[k];
             
-            // if (k==0)
-            // {
-                // m1[k] = 0.2*sout1[k];
-                // m2[k] = m1[k];	m3[k] = m2[k];
-                // m4[k] = m3[k];	m5[k] = m4[k];
-            // }
-            // else if (k==1)
-            // {
-                // m1[k] = 0.491115852967412*m1[k-1] + 0.2*(sout1[k] - 0.173492003319319*sout1[k-1]);
-                // m2[k] = 1.084520302502860*m2[k-1] + m1[k] - 0.803462163297112*m1[k-1];
-                // m3[k] = 1.588427084535629*m3[k-1] + m2[k] - 1.416084732997016*m2[k-1];
-                // m4[k] = 1.886287488516458*m4[k-1] + m3[k] - 1.830362725074550*m3[k-1];
-                // m5[k] = 1.989549282714008*m5[k-1] + m4[k] - 1.983165053215032*m4[k-1];
-            // }
-            // else
-            // {
-                // m1[k] = 0.491115852967412*m1[k-1] - 0.055050209956838*m1[k-2]+ 0.2*(sout1[k]- 0.173492003319319*sout1[k-1]+ 0.000000172983796*sout1[k-2]);
-                // m2[k] = 1.084520302502860*m2[k-1] - 0.288760329320566*m2[k-2] + m1[k] - 0.803462163297112*m1[k-1] + 0.154962026341513*m1[k-2];
-                // m3[k] = 1.588427084535629*m3[k-1] - 0.628138993662508*m3[k-2] + m2[k] - 1.416084732997016*m2[k-1] + 0.496615555008723*m2[k-2];
-                // m4[k] = 1.886287488516458*m4[k-1] - 0.888972875389923*m4[k-2] + m3[k] - 1.830362725074550*m3[k-1] + 0.836399964176882*m3[k-2];
-                // m5[k] = 1.989549282714008*m5[k-1] - 0.989558985673023*m5[k-2] + m4[k] - 1.983165053215032*m4[k-1] + 0.983193027347456*m4[k-2];
-            // }
-            // I1 = m5[k];
-        // } /* end of approximate implementation */
+            if (k==0)
+            {
+                m1[k] = 0.2*sout1[k];
+                m2[k] = m1[k];	m3[k] = m2[k];
+                m4[k] = m3[k];	m5[k] = m4[k];
+            }
+            else if (k==1)
+            {
+                m1[k] = 0.491115852967412*m1[k-1] + 0.2*(sout1[k] - 0.173492003319319*sout1[k-1]);
+                m2[k] = 1.084520302502860*m2[k-1] + m1[k] - 0.803462163297112*m1[k-1];
+                m3[k] = 1.588427084535629*m3[k-1] + m2[k] - 1.416084732997016*m2[k-1];
+                m4[k] = 1.886287488516458*m4[k-1] + m3[k] - 1.830362725074550*m3[k-1];
+                m5[k] = 1.989549282714008*m5[k-1] + m4[k] - 1.983165053215032*m4[k-1];
+            }
+            else
+            {
+                m1[k] = 0.491115852967412*m1[k-1] - 0.055050209956838*m1[k-2]+ 0.2*(sout1[k]- 0.173492003319319*sout1[k-1]+ 0.000000172983796*sout1[k-2]);
+                m2[k] = 1.084520302502860*m2[k-1] - 0.288760329320566*m2[k-2] + m1[k] - 0.803462163297112*m1[k-1] + 0.154962026341513*m1[k-2];
+                m3[k] = 1.588427084535629*m3[k-1] - 0.628138993662508*m3[k-2] + m2[k] - 1.416084732997016*m2[k-1] + 0.496615555008723*m2[k-2];
+                m4[k] = 1.886287488516458*m4[k-1] - 0.888972875389923*m4[k-2] + m3[k] - 1.830362725074550*m3[k-1] + 0.836399964176882*m3[k-2];
+                m5[k] = 1.989549282714008*m5[k-1] - 0.989558985673023*m5[k-2] + m4[k] - 1.983165053215032*m4[k-1] + 0.983193027347456*m4[k-2];
+            }
+            I1 = m5[k];
+         } /* end of approximate implementation */
         
-        // synSampOut[k] = sout1[k] + sout2[k];
-        // k = k+1;
-    // }   /* end of all samples */
-    // mexPrintf("Here5\n");
-    // mxFree(sout1); mxFree(sout2);
-    // mxFree(m1); mxFree(m2); mxFree(m3); mxFree(m4); mxFree(m5); mxFree(n1); mxFree(n2); mxFree(n3);
+        synSampOut[k] = sout1[k] + sout2[k];
+        k = k+1;
+    }   /* end of all samples */
+    mxFree(sout1); mxFree(sout2);
+    mxFree(m1); mxFree(m2); mxFree(m3); mxFree(m4); mxFree(m5); mxFree(n1); mxFree(n2); mxFree(n3);
     // /*----------------------------------------------------------*/
     // /*----- Upsampling to original (High 100 kHz) sampling rate --------*/
     // /*----------------------------------------------------------*/
-    // for(z=0; z<k-1; ++z)
-    // {
-        // incr = (synSampOut[z+1]-synSampOut[z])/resamp;
-        // for(b=0; b<resamp; ++b)
-        // {
-            // TmpSyn[z*resamp+b] = synSampOut[z]+ b*incr;
-        // }
-    // }
-    // for (i=0;i<totalstim*nrep;++i)
-        // synout[i] = TmpSyn[i+delaypoint];
-     mexPrintf("Here6\n");
+    for(z=0; z<k-1; ++z)
+    {
+        incr = (synSampOut[z+1]-synSampOut[z])/resamp;
+        // Catch whether the loop will attempt to write beyond the bounds of the array
+        if (z*resamp+resamp-1 > totalstim*nrep+2*delaypoint)
+        {
+          mexPrintf("Matrix dimensions exceeded...\n");
+          break;
+        }
+        else
+          for(b=0; b<resamp; ++b)
+            TmpSyn[z*resamp+b] = synSampOut[z]+ b*incr;
+    }
+    for (i=0;i<totalstim*nrep;++i)
+      synout[i] = TmpSyn[i+delaypoint];
     
     mxFree(synSampOut); mxFree(TmpSyn);
     mxDestroyArray(randInputArray[0]); mxDestroyArray(randOutputArray[0]);
