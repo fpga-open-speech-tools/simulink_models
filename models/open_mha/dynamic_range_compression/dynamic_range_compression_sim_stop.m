60691de6f55ea8cfd212e2ccbcb88ae7bd3b2f4c
Connor Dack <dack@flatearthinc.com>
2020-12-10 13:09:47 -0700

    Updated the Dynamic Range Compression Simulatuion
    Added Dataplane Subsystem to Generate HDL
    Added Debug Mode

--- models/open_mha/dynamic_range_compression/dynamic_range_compression_sim_stop.m
+++ models/open_mha/dynamic_range_compression/dynamic_range_compression_sim_stop.m
@@ -14,12 +14,45 @@
 % Bozeman, MT 59718
 % openspeech@flatearthinc.com
 
-%% Plot the Results
-figure;
-subplot(2,1,1)
-plot(modified_FFT_data_real)
-title('FFT Data: Real')
+%% Calculate the Results
 
-subplot(2,1,2)
-plot(modified_FFT_data_imag)
-title('FFT Data: Imaginary')
+%% Plot the Results
+if debug == true
+    figure;
+    subplot(3,1,1)
+    plot(drc_gain_sim_out)
+    legend('Simulink')
+    title('DRC Gain Simulation')
+    
+    subplot(3,1,2)
+    plot(modified_FFT_data_real)
+    legend('Simulink')
+    title('FFT Data: Real')
+
+    subplot(3,1,2)
+    plot(modified_FFT_data_imag)
+    legend('Simulink')
+    title('FFT Data: Imaginary')
+    
+else
+    figure;
+    subplot(4,1,1)
+    plot(modified_FFT_data_real)
+    legend('Simulink')
+    title('FFT Data: Real')
+
+    subplot(4,1,2)
+    plot(modified_FFT_data_imag)
+    legend('Simulink')
+    title('FFT Data: Imaginary')
+    
+    subplot(4,1,3)
+    plot(fft_valid_sim_out)
+    legend('Simulink')
+    title('FFT Valid Simulation')
+    
+    subplot(4,1,4)
+    plot(fft_frame_pulse_sim_out)
+    legend('Simulink')
+    title('FFT Frame Pulse Simulation')
+end
