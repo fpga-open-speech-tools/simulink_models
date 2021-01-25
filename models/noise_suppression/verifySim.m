4717067e26c54a5a3117d5e969375745a5619e30
Connor Dack <dack@flatearthinc.com>
2021-01-25 12:23:26 -0700

    Added verification compared to the adaptive wiener filter function

--- models/noise_suppression/verifySim.m
+++ models/noise_suppression/verifySim.m
@@ -21,32 +21,72 @@
 % FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 % ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 %
-% Ross K. Snider
-% Audio Logic
+% Connor Dack
+% AudioLogic, Inc
 % 985 Technology Blvd
 % Bozeman, MT 59718
 % openspeech@flatearthinc.com
 
-%% Verify that the test data got encoded, passed through the model, and
-% decoded correctly.  The input (modified by gain) and output values should be identical.
-
+%% Verify the results of the Noise Suppresion Models
+% Initialize
 close all;
+original_audio = AudioSource.fromFile([mp.test_signals_path filesep 'sp03.wav'], mp.Fs, mp.nSamples);
+noiseVariance = mp.register{2}.timeseries.Data(1);
+% Calculate the MATLAB Results
+[left_channel_matlab, ~]  = adaptiveWienerFilt(testSignal.audio(:,1), mp.Fs, mp.windowSize, noiseVariance);
+[right_channel_matlab, ~] = adaptiveWienerFilt(testSignal.audio(:,2), mp.Fs, mp.windowSize, noiseVariance);
 
-figure(1)
-subplot(2,1,1)
-plot(testSignal.audio(:,1)); hold on
+% Compute the SNR Metrics for the Left Channel
+original_audio = original_audio.audio;
+simulink_audio = double(dataOut.Data(1:mp.nSamples,1));
+snr_simulink   = snr(original_audio, simulink_audio - original_audio);
+disp(['SNR Simulink: ' num2str(snr_simulink)])
+snr_matlab = snr(original_audio, left_channel_matlab - original_audio);
+disp(['SNR MATLAB: ' num2str(snr_matlab)])
+
+% Windowed SNR
+[segmental_snr_simulink, snr_simulink_mean] = segmentalSnr(simulink_audio, original_audio, mp.windowSize);
+disp(['SNR Simulink Mean: ' num2str(snr_simulink_mean)])
+[segmental_snr_matlab, snr_matlab_mean] = segmentalSnr(left_channel_matlab, original_audio, mp.windowSize);
+disp(['SNR MATLAB Mean: ' num2str(snr_matlab_mean)])
+%% Plot the Results
+% Left Channel
+figure
+subplot(4,1,1)
+plot(original_audio)
+title('Left Channel Original Input')
+
+subplot(4,1,2)
+plot(testSignal.audio(:,1));
+title('Left Channel Noisy Input')
+
+subplot(4,1,3)
 plot(dataOut.Data(:,1))
-title('Left channel')
-legend('input', 'output')
+hold on
+plot(left_channel_matlab, '--')
+title('Left Channel Filtered Results')
+legend('Simulink', 'MATLAB')
 
-subplot(2,1,2)
-plot(testSignal.audio(:,2)); hold on
+subplot(4,1,4)
+plot(segmental_snr_simulink)
+hold on
+plot(segmental_snr_matlab, '--')
+title('SNR over the Window Size')
+legend('Simulink', 'MATLAB')
+
+% Right Channel
+figure
+subplot(3,1,1)
+plot(original_audio)
+title('Right Channel Original Input')
+
+subplot(3,1,2)
+plot(testSignal.audio(:,2));
+title('Right Channel Noisy Input')
+
+subplot(3,1,3)
 plot(dataOut.Data(:,2))
-title('Right channel')
-legend('input', 'output')
-
-% original_audio = [mp.test_signal.left(:) mp.test_signal.right(:)];
-% processed_audio = [mp.left_data_out(:) mp.right_data_out(:)];
-% soundsc(original_audio, mp.Fs);
-% pause(mp.test_signal.duration*1.1);
-% soundsc(processed_audio, mp.Fs);
+hold on
+plot(right_channel_matlab,'--')
+title('Right Channel Filtered Results')
+legend('Simulink', 'MATLAB')
