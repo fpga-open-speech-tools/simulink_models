ebe63e9121a8654631de2452623318424f7ba377
Connor Dack <dack@flatearthinc.com>
2021-01-25 12:22:00 -0700

    Updated validation files

--- models/noise_suppression/MATLAB_scripts/adaptiveWienerFilt.m
+++ models/noise_suppression/MATLAB_scripts/adaptiveWienerFilt.m
@@ -1,13 +1,13 @@
 function [sn, means] = adaptiveWienerFilt(noisyAudio, Fs, windowSize, noiseVariance)
-%%Adaptive Wiener Filter
+%% Adaptive Wiener Filter
 % Author: Justin Williams
-% Under:  Audio Logic
-% NIH Speech Enhancement Research
+% Under:  AudioLogic, Inc
+%         Frost Examples
 %
-%%This function imeplements an adaptive wiener filter that attenuates white
-%%noise by using sample-based statistical approximation. It uses a
-%%look-behind window to keep track of statistical variance and means to
-%%estimate the SNR(?) and filters the signal sample-by-sample.
+% This function imeplements an adaptive wiener filter that attenuates white
+% noise by using sample-based statistical approximation. It uses a
+% look-behind window to keep track of statistical variance and means to
+% estimate the SNR(?) and filters the signal sample-by-sample.
 %
 % Inputs:
 %           noisyAudio  - Noisy speech signal
@@ -15,22 +15,21 @@ function [sn, means] = adaptiveWienerFilt(noisyAudio, Fs, windowSize, noiseVaria
 %           windowSize  - Size of Window [0 5] ms
 % Outputs:
 %           sn          - Filtered Output Signal
-
+%% Calculations
 Ts = 1 / Fs;
 sn = zeros(length(noisyAudio),1 );
-winSize = round(windowSize / Ts);
-win = zeros(winSize,1);        % Init look-behind-window
-means = zeros(ceil(length(noisyAudio)/winSize), 1);
-meansExponential = zeros(ceil(length(noisyAudio)/winSize), 1);
-varExponential = zeros(ceil(length(noisyAudio)/winSize), 1);
-exponentialWeight = 2/(winSize + 1);
+win = zeros(windowSize,1);        % Init look-behind-window
+means = zeros(ceil(length(noisyAudio)/windowSize), 1);
+meansExponential = zeros(ceil(length(noisyAudio)/windowSize), 1);
+varExponential = zeros(ceil(length(noisyAudio)/windowSize), 1);
+exponentialWeight = 2/(windowSize + 1);
 for n = 1:length(noisyAudio)
     
-    if n <= winSize
+    if n <= windowSize
         win(n)    = noisyAudio(n);
         [signalAvg, signalVariance] = wienStats(win, noiseVariance);
-    elseif (n > winSize && n <= (length(noisyAudio) - winSize))
-        win     = noisyAudio(n - winSize + 1 : n);
+    elseif (n > windowSize && n <= (length(noisyAudio) - windowSize))
+        win     = noisyAudio(n - windowSize + 1 : n);
         [signalAvg, signalVariance] = wienStats(win, noiseVariance);
     else
         win     = noisyAudio((n):end);
@@ -56,7 +55,6 @@ for n = 1:length(noisyAudio)
         sn(n) = 0;
     end
 end
-disp('done')
                 
 %% Calculate Stats 
 function [signalAvg, signalVariance] = wienStats(win, noiseVariance)
