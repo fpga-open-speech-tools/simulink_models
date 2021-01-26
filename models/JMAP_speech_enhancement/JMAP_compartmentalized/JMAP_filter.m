71451d081e6cdb9d8ed357db287c43a53f5a5b05
Connor Dack <dack@flatearthinc.com>
2021-01-26 12:04:23 -0700

    Removed the JMAP Speech Enhancement Project

--- models/JMAP_speech_enhancement/JMAP_compartmentalized/JMAP_filter.m
+++
@@ -1,9 +0,0 @@
-function [ensig, Xk_prev] = JMAP_filter(N, hw, frameMag, nFFT)
-H = zeros(nFFT, 1);
-H(1:N) = 1/N;                               % FIR Filter Coefficients
-HPF = conv(H,abs(hw));                      % FIR Filter Declaration
-ensig = frameMag.*HPF(1:length(frameMag));  % Filtering endSignal
-    
-Xk_prev = ensig.^2;  % postSNR estimation reused for next frame 
-
-end
