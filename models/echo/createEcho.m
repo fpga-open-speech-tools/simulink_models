cc690e682cc511027d2ec3ad623713cf32b4e11f
Tyler Davis <davis@flatearthinc.com>
2021-01-26 11:01:47 -0700

    Updated calculation method

--- models/echo/createEcho.m
+++ models/echo/createEcho.m
@@ -1,9 +1,22 @@
 function audioOutput = createEcho(data,wetDryRatio,feedbackGain,echoDuration)
-  
-  echoSignal = zeros(size(data));
-  
-  echoSignal((echoDuration+1):end) = feedbackGain*data(1:(end-echoDuration));
-  
-  audioOutput = wetDryRatio*data + (1-wetDryRatio)*echoSignal;
+  % This function is based off the block diagram for the Delay-Based Audio Effects example from Mathworks
 
+  data = data';
+  echoSignal = zeros(size(data));
+      
+  % Loop through the array
+  for ii = echoDuration:length(data)
+    
+    echoSignal(ii) = feedbackGain.*echoSignal(ii-echoDuration+1) + data(ii);
+    
+    if echoSignal(ii) > 1
+      echoSignal(ii) = 1;
+    elseif echoSignal(ii) < -1
+      echoSignal(ii) = -1;
+    end
+  end
+  audioOutput = wetDryRatio*data + (1-wetDryRatio)*echoSignal;
+  
+  audioOutput(audioOutput > 1) = 1;
+  audioOutput(audioOutput < -1) = -1;
 end
