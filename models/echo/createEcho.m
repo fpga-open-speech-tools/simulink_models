8074b76c41fc55b66660448e6edd9e5b2494d6dc
Tyler Davis <davis@flatearthinc.com>
2021-01-26 11:38:56 -0700

    Added comments, cleaned up code

--- models/echo/createEcho.m
+++ models/echo/createEcho.m
@@ -1,21 +1,24 @@
 function audioOutput = createEcho(data,wetDryRatio,feedbackGain,echoDuration)
   % This function is based off the block diagram for the Delay-Based Audio Effects example from Mathworks
 
+  % Initialize the echo array
   echoSignal = zeros(size(data));
       
-  % Loop through the array
-  for ii = echoDuration+1:length(data)-1
+  % Loop through the array and apply the feedback
+  for ii = echoDuration+2:length(data)
     
-    echoSignal(ii+1) = feedbackGain.*echoSignal(ii-echoDuration) + data(ii-echoDuration+1);
+    echoSignal(ii) = feedbackGain.*echoSignal(ii-echoDuration-1) + data(ii-echoDuration);
     
+    % Ensure the audio is bounded between -1 and 1
     if echoSignal(ii) > 1
       echoSignal(ii) = 1;
     elseif echoSignal(ii) < -1
       echoSignal(ii) = -1;
     end
+    
   end
+  
+  % Combine the original audio (dry) and the echo signal (wet)
   audioOutput = (1-wetDryRatio)*data + wetDryRatio*echoSignal;
   
-  audioOutput(audioOutput > 1) = 1;
-  audioOutput(audioOutput < -1) = -1;
 end
