832b4da0ff91d34097fcb6118eeec3b09854d745
Dylan Wickham <61513109+fe-wickham@users.noreply.github.com>
2021-01-26 14:41:57 -0700

    Updated simple gain to 24 23 codec  (#63)
    * Updated simple gain to 24 23 codec with registers that don't have 'register_control' in their name in the model
    
    * Removed the 2019b simulink model
    
    * Updated the simple gain model
    
    Updated the Model JSON
    Added Verification
    
    * Initial Commit
    
    Co-authored-by: Connor Dack <dack@flatearthinc.com>

--- models/simple_gain/modelparameters.m
+++ models/simple_gain/modelparameters.m
@@ -1,11 +1,8 @@
 
 %% Autogen parameters
-
-mp.testFile = [mp.test_signals_path filesep 'noisySpeech.wav'];
-
-% TODO: use booleans instead of 0 and 1
+mp.testFile    = [mp.test_signals_path filesep 'acoustic.wav'];
 mp.sim_prompts = 1;
-mp.sim_verify = 0;
-mp.simDuration = 5;
-mp.nSamples = config.system.sampleClockFrequency * mp.simDuration;
+mp.sim_verify  = 0;
+mp.simDuration = .05;
+mp.nSamples    = config.system.sampleClockFrequency * mp.simDuration;
 
