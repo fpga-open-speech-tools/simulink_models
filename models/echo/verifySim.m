0dacf67ab797d7203c1f8b5c8aceab2125282043
Tyler Davis <davis@flatearthinc.com>
2021-01-26 13:20:56 -0700

    Cleaned up comments, removed unused lines

--- models/echo/verifySim.m
+++ models/echo/verifySim.m
@@ -30,10 +30,12 @@
 %% Verify that the test data got encoded, passed through the model, and
 % decoded correctly.  The input (modified by gain) and output values should be identical.
 
+% Get the left and right audio signals
 audioInputLeft  = testSignal.audio(:,1);
-audioOutputLeft = createEcho(audioInputLeft,wetDryRatio,feedbackGain,echoDuration);
-
 audioInputRight  = testSignal.audio(:,2);
+
+% Calculate the echo result using a Matlab script
+audioOutputLeft = createEcho(audioInputLeft,wetDryRatio,feedbackGain,echoDuration);
 audioOutputRight = createEcho(audioInputRight,wetDryRatio,feedbackGain,echoDuration);
 
 figure(1)
@@ -54,9 +56,3 @@ plot(audioOutputRight,'--');
 hold off
 title(['Delay = ' num2str(mp.register{2}.value) '  Enable = ' num2str(mp.register{1}.value) '  Decay = ' num2str(mp.register{3}.value)  '  Wet/Dry Mix = ' num2str(mp.register{4}.value)])
 legend('Input Audio','Simulink Simulation','Direct Calculation')
-
-% original_audio = [mp.test_signal.left(:) mp.test_signal.right(:)];
-% processed_audio = [mp.left_data_out(:) mp.right_data_out(:)];
-% soundsc(original_audio, mp.Fs);
-% pause(mp.test_signal.duration*1.1);
-% soundsc(processed_audio, mp.Fs);
