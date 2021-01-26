a57589d2422ef16e258eaf7f603e5fccab8c8ec9
Connor Dack <dack@flatearthinc.com>
2021-01-26 11:55:05 -0700

    Deleted Compression

--- models/compression/Initialize_Delay_Values.m
+++
@@ -1,6 +0,0 @@
-%Initialize_Delay_Values
-
-% Find maxiumum delay of all bandpass filters
-delays = floor(calib_bpfs(1,:)/2);
-maxDelay = max(delays);
-delayFix = maxDelay-delays;
--- models/compression/hdlsrc/DynamicCompressionModel/LogAddressing_compile.do
+++
@@ -1,4 +0,0 @@
-vlib work
-vcom  LSL_N.vhd
-vcom  LZC32_BLACKBOX.vhd
-vcom  LogAddressing.vhd
